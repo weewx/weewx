ec50650a2b3aaf5530e757891a54f09be55b5449
Tom Keffer <tkeffer>
2009-11-14 01:12:12 +0000

    Update rc1

---
+++ trunk/weeutil/__init__.py
@@ -0,0 +1,9 @@
+#
+#    Copyright (c) 2009 Tom Keffer <tkeffer@gmail.com>
+#
+#    See the file LICENSE.txt for your full rights.
+#
+#    Revision: $Rev$
+#    Author:   $Author$
+#    Date:     $Date$
+#
