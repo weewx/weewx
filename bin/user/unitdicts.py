5b330cd78154b8a15c37bdd307f9887bdd050d39
Tom Keffer <tkeffer>
2011-01-20 01:11:19 +0000

    New module dedicated to user extensions.

---
+++ bin/user/unitdicts.py
@@ -0,0 +1,18 @@
+#
+#    Copyright (c) 2011 Tom Keffer <tkeffer@gmail.com>
+#
+#    See the file LICENSE.txt for your full rights.
+#
+#    $Revision$
+#    $Author$
+#    $Date$
+#
+"""User extensions to the unit dictionaries."""
+
+import weewx.unitdicts
+
+#===============================================================================
+# This would be the place to add or modify the unit dictionaries.
+# Any changes you make here will carry forward in new versions of 
+# weewx.
+#===============================================================================
