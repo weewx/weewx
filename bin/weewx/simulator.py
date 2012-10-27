94987b5d1ceafb7212e3e2225254bf5d7d794183
Tom Keffer <tkeffer>
2012-10-27 21:48:34 +0000

    Added wind and rain to the simulator

--- bin/weewx/simulator.py
+++ bin/weewx/simulator.py
@@ -9,19 +9,39 @@
 #
 """Console simulator for the weewx weather system"""
 
+from __future__ import with_statement
 import math
 import time
 
+import weedb
 import weeutil.weeutil
 import weewx.abstractstation
+import weewx.wxformulas
 
 def loader(config_dict):
 
+    # This loader uses a bit of a hack to have the simulator resume at a time
+    # after the last record in the database. It's not bad, but I'm not
+    # enthusiastic about having special knowledge about the database in a
+    # driver, albeit just the loader.
+
+    start_ts = None
     if config_dict['Simulator'].has_key('start'):
-        start_tt = time.strptime(config_dict['Simulator']['start'], "%Y-%m-%d %H:%M")
-        start_ts = time.mktime(start_tt)
-    else:
-        start_ts = None
+        # A start time has been specified. Resume with the last time in the
+        # database. If there is no such time, use the time specified in the
+        # configuration dictionary.
+        archive_db = config_dict['StdArchive']['archive_database']        
+        archive_db_dict = config_dict['Databases'][archive_db]
+        try:
+            with weewx.archive.Archive.open(archive_db_dict) as archive:
+                start_ts = archive.lastGoodStamp()
+        except weedb.OperationalError:
+            pass
+        if start_ts is None:
+            # No database, or it has no data in it. Use the specified time.
+            start_tt = time.strptime(config_dict['Simulator']['start'], "%Y-%m-%d %H:%M")
+            start_ts = time.mktime(start_tt)
+            
     station = Simulator(start_ts=start_ts, **config_dict['Simulator'])
     
     return station
@@ -50,9 +70,16 @@ class Simulator(weewx.abstractstation.AbstractStation):
         
         sod = weeutil.weeutil.startOfDay(self.the_time)
                 
-        self.observations = {'outTemp'   : Observation(magnitude=20.0, average=50.0, period=24.0, phase_lag=14.0, start=sod),
-                             'inTemp'    : Observation(magnitude=5.0,  average=68.0, period=24.0, phase_lag=12.0, start=sod),
-                             'barometer' : Observation(magnitude=1.0,  average=30.1, period=96.0, phase_lag=48.0, start=sod)}
+        # The following doesn't make much meteorological sense, but it is easy to program!
+        self.observations = {'outTemp'    : Observation(magnitude=20.0,  average= 50.0, period=24.0, phase_lag=14.0, start=sod),
+                             'inTemp'     : Observation(magnitude=5.0,   average= 68.0, period=24.0, phase_lag=12.0, start=sod),
+                             'barometer'  : Observation(magnitude=1.0,   average= 30.1, period=96.0, phase_lag=48.0, start=sod),
+                             'windSpeed'  : Observation(magnitude=10.0,  average=  5.0, period=96.0, phase_lag=24.0, start=sod),
+                             'windDir'    : Observation(magnitude=360.0, average=180.0, period=96.0, phase_lag= 0.0, start=sod),
+                             'windGust'   : Observation(magnitude=12.0,  average=  6.0, period=96.0, phase_lag=24.0, start=sod),
+                             'windGustDir': Observation(magnitude=360.0, average=180.0, period=96.0, phase_lag= 0.0, start=sod),
+                             'humidity'   : Observation(magnitude=30.0,  average= 50.0, period=96.0, phase_lag= 0.0, start=sod),
+                             'rain'       : Rain(rain_start=0, rain_length=3, total_rain=0.2, loop_interval=self.loop_interval)}
 
     def genLoopPackets(self):
 
@@ -84,28 +111,13 @@ class Simulator(weewx.abstractstation.AbstractStation):
             for obs_type in self.observations:
                 _packet[obs_type] = self.observations[obs_type].value_at(avg_time)
 
+            _packet['windchill'] = weewx.wxformulas.windchillF(_packet['outTemp'], _packet['windSpeed'])
+            _packet['heatindex'] = weewx.wxformulas.heatindexF(_packet['outTemp'], _packet['humidity']) 
             yield _packet
 
-                
-    def genArchiveRecords(self, lastgood_ts):
-        # What this will do is bring the simulator in synch with the last good
-        # time in the database. The result is that it will resume starting at
-        # that time.
-        if lastgood_ts:
-            self.the_time = lastgood_ts
-        # However, it does not actually emit any records, so we still have
-        # to signal that there is no implementation.
-        raise NotImplementedError
-    
     def getTime(self):
         return self.the_time
     
-    def setTime(self, newtime_ts):
-        # If the Simulator is not running in real time, you don't want to
-        # change the time.
-        if self.start_ts is None:
-            self.the_time = newtime_ts
-        
     @property
     def hardware_name(self):
         return "Simulator"
@@ -138,6 +150,30 @@ class Observation(object):
         return self.magnitude * math.cos(phase) + self.average
         
 
+class Rain(object):
+    
+    bucket_tip = 0.01
+
+    def __init__(self, rain_start=0, rain_length=1, total_rain=0.1, loop_interval=None):
+        """Initialize a rain simulator"""
+        npackets = 3600 * rain_length / loop_interval
+        n_rain_packets = total_rain / Rain.bucket_tip
+        self.period = int(npackets/n_rain_packets)
+        self.rain_start = 3600* rain_start
+        self.rain_end = rain_start + 3600 * rain_length
+        self.packet_number = 0
+        
+    def value_at(self, time_ts):
+        time_tt = time.localtime(time_ts)
+        secs_since_midnight = time_tt.tm_hour * 3600 + time_tt.tm_min * 60.0 + time_tt.tm_sec
+        if self.rain_start < secs_since_midnight <= self.rain_end:
+            amt = Rain.bucket_tip if self.packet_number % self.period == 0 else 0.0
+            self.packet_number += 1
+        else:
+            self.packet_number = 0
+            amt = 0
+        return amt
+        
 
 
 if __name__ == "__main__":
