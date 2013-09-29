5382f8158dafbf9814ebbeb66aa76a96e2542e91
Tom Keffer <tkeffer>
2013-09-29 14:54:44 +0000

    Got directory and module extension loader working.

--- bin/weeutil/extension.py
+++ bin/weeutil/extension.py
@@ -10,53 +10,88 @@
 """Routines for the weewx extension facilities."""
 
 import inspect
-import sys
 import os
 import os.path
+import sys
 
 class ModuleExtensionLoader(object):
-    
+    """Manipulates a module."""
+
     def __init__(self, module_name):
-        
+        """Import a module by name."""
         self.module_name = module_name
         __import__(module_name)
         self.module = sys.modules[module_name]
-        
-    def discover_classes(self, superclass):
 
-        _mems = []       
+    def discover_classes(self, superclass):
+        """Discover all classes in a module that are the subclass of a given superclass.
+        
+        superclass: All strict subclasses within the module will be returned.
+        
+        returns:
+        A list of tuples (symname, Cls) where symname is a symbolic name and Cls is a 
+        class object.
+        """
+
+        _mems = []
+        # Get all the entries in the module:
         for _name, _Cls in inspect.getmembers(self.module):
+            # Find all entries that are a strict subclass of the superclass:
             if inspect.isclass(_Cls) and issubclass(_Cls, superclass) and _Cls != superclass:
+                # Retrieve a symbolic name for the class. If not found, use the class name
                 _symname = _Cls.symname if hasattr(_Cls, 'symname') else _name
                 _mems.append((_symname, _Cls))
-        
-        _mems.sort(key=lambda x:x[1], reverse=False)
         return _mems
 
 class DirectoryExtensionLoader(object):
-    
+    """Manipulates modules found in a directory."""
+
     def __init__(self, dir_name):
-        
-        old_path = list(sys.path)
-        sys.path.append(dir_name)
-        
+        """Loads all the modules found in a directory."""
+        # Get all the files found in the directory.
+        _files = os.listdir(dir_name)
+        # Because listdir() returns a list in arbitrary order, sort it.
+        _files.sort()
+
         self.module_list = []
-        for _file in os.listdir(dir_name):
-            if os.path.isfile(os.path.join(dir_name, _file)):
-                _file_info = os.path.splitext(_file)
-                if _file_info[1] == ".py":
+
+        # Go through all the files that were found
+        for _file in _files:
+            # Get the full path for each file:
+            _file_path = os.path.join(dir_name, _file)
+            # Follow any symbolic links:
+            _real_path = os.path.realpath(_file_path)
+            # Make sure it's a real file (not a directory):
+            if os.path.isfile(_real_path):
+                # Split out the suffix
+                _file_info = os.path.splitext(os.path.basename(_real_path))
+                # Accept only those files with a suffix ".py" or no suffix at all:
+                if _file_info[1] in [".py", '']:
+                    # Massage the path so that the module can be found, saving the
+                    # old path first
+                    _old_path = list(sys.path)
+                    sys.path.append(os.path.dirname(_real_path))
+                    # Add the retrieved module to the list of modules
                     self.module_list.append(ModuleExtensionLoader(_file_info[0]))
-            
-        sys.path = old_path
-            
+                    sys.path = _old_path
+
     def discover_classes(self, superclass):
+        """Discover all classes in a directory that are the subclass of a given superclass.
         
-        _mem_set = set()
+        superclass: All strict subclasses within the modules will be returned.
+        
+        returns:
+        A list of tuples (symname, Cls) where symname is a symbolic name and Cls is a 
+        class object.
+        """
+
+        _mems = []
+        # Go through all the modules in the list.
         for _module in self.module_list:
+            # For each module, find all subclasses of the given superclass
             for _symname, _Cls in _module.discover_classes(superclass):
-                _mem_set.add((_symname, _Cls))
-                
-        _mems = list(_mem_set)
-        _mems.sort(key=lambda x:x[1], reverse=False)
+                # Add any that were found to the list
+                _mems.append((_symname, _Cls))
+
         return _mems
 
