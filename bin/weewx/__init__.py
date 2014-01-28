69e5dacc74109c8b014c2f171dcc242d9523227f
Matthew Wall <mwall>
2014-01-28 17:50:49 +0000

    bump version to 2.6.0a6

--- bin/weewx/__init__.py
+++ bin/weewx/__init__.py
@@ -8,7 +8,7 @@
 """Package weewx, containing modules specific to the weewx runtime engine."""
 import time
 
-__version__="2.6.0a5"
+__version__="2.6.0a6"
 
 # Holds the program launch time in unix epoch seconds:
 # Useful for calculating 'uptime.'
