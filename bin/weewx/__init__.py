2e562dc78e746298c2b5c8a4d1ba0128d558d58e
Tom Keffer <tkeffer>
2014-02-08 00:44:13 +0000

    v2.6.0

--- bin/weewx/__init__.py
+++ bin/weewx/__init__.py
@@ -8,7 +8,7 @@
 """Package weewx, containing modules specific to the weewx runtime engine."""
 import time
 
-__version__="2.6.0b2"
+__version__="2.6.0"
 
 # Holds the program launch time in unix epoch seconds:
 # Useful for calculating 'uptime.'
