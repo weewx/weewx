48f8671585c03531114e0aa6783ec3d92d4fa831
Tom Keffer <tkeffer>
2014-11-04 18:59:25 +0000

    Adopted the "tocify" table of contents generator, more suitable to long documents.

---
+++ docs/js/weewx.js
@@ -0,0 +1,26 @@
+/*
+ *  Copyright (c) 2014 Tom Keffer <tkeffer@gmail.com>
+ * 
+ *  See the file LICENSE.txt for your full rights.
+ *
+ *  $Id$
+ */
+
+function genIds(text, element) {
+	// Given an element, extracts a suitable ID tag, for use in
+	// links.
+    hv = element[0].getAttribute('id');
+    if (hv == null) {
+      // prettify the text
+      hv = text.toLowerCase().replace(/\s/g, "-");
+      // fix double hyphens
+      while (hv.indexOf("--") > -1) {
+        hv = hv.replace(/--/g, "-");
+      }
+      // fix colon-space instances
+      while (hv.indexOf(":-") > -1) {
+        hv = hv.replace(/:-/g, "-");
+      }
+    }
+    return hv;
+}
