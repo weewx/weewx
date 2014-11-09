c3ae4c10de2c8f1c65367dae9c9054c16924f626
Tom Keffer <tkeffer>
2014-11-09 14:45:59 +0000

    Put filegenerator.py back in. It's needed to maintain backwards compatibility with skin.conf.

---
+++ bin/weewx/filegenerator.py
@@ -0,0 +1,14 @@
+#
+#    Copyright (c) 2009, 2010, 2013 Tom Keffer <tkeffer@gmail.com>
+#
+#    See the file LICENSE.txt for your full rights.
+#
+#    $Revision$
+#    $Author$
+#    $Date$
+#
+
+import weewx.cheetahgenerator
+
+# For backwards compatibility:
+FileGenerator = weewx.cheetahgenerator.CheetahGenerator
