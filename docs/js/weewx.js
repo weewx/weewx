f55816c757577513dcf9755dd2168559f5dc8f23
Matthew Wall <mwall>
2014-11-13 15:42:19 +0000

    temporary two-step toc level controller

--- docs/js/weewx.js
+++ docs/js/weewx.js
@@ -16,3 +16,22 @@ function wee_gen_id(text, element) {
     }
     return hv
 }
+
+function set_cookie(name, value, dur) {
+    if(dur==null || dur==0) dur=30;
+    var today = new Date();
+    var expire = new Date();
+    expire.setTime(today.getTime() + 24*3600000*dur);
+    document.cookie = name+"="+escape(value)+";expires="+expire.toGMTString();
+}
+
+function get_cookie(name) {
+    if(name=="") return;
+    var cookie = " "+document.cookie;
+    var i = cookie.indexOf(" "+name+"=");
+    if(i<0) i = cookie.indexOf(";"+name+"=");
+    if(i<0) return "";
+    var j = cookie.indexOf(";", i+1);
+    if(j<0) j = cookie.length;
+    return unescape(cookie.substring(i + name.length + 2, j));
+}
