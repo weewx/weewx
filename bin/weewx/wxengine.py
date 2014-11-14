bfe45cff5db5d2b984ed5405394da66f7014ff8c
Tom Keffer <tkeffer>
2014-11-14 02:32:29 +0000

    Added a stub for weewx.wxengine for backwards compatibility. Seems to be the only thing that kept my site from being backwards compatible!

---
+++ bin/weewx/wxengine.py
@@ -0,0 +1,11 @@
+#
+#    Copyright (c) 2009-2014 Tom Keffer <tkeffer@gmail.com>
+#
+#    See the file LICENSE.txt for your full rights.
+#
+#    $Id: engine.py 2598 2014-11-14 01:14:25Z tkeffer $
+
+import weewx.engine
+
+# For backwards compatibility:
+StdService = weewx.engine.StdService
