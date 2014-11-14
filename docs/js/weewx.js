e7bfd7e877424c201fb8932e60874090fe9a9e36
Matthew Wall <mwall>
2014-11-14 02:45:33 +0000

    added select menu for toc detail

--- docs/js/weewx.js
+++ docs/js/weewx.js
@@ -7,7 +7,7 @@
  */
 
 function wee_gen_id(text, element) {
-	// If the element has an explicit id, use that.
+        // If the element has an explicit id, use that.
 	// Otherwise, use the same generator function as the
 	// old Samaxes TOC library
 	hv = element[0].getAttribute('id');
@@ -26,7 +26,7 @@ function set_cookie(name, value, dur) {
 }
 
 function get_cookie(name) {
-    if(name=="") return;
+    if(name=="") return "";
     var cookie = " "+document.cookie;
     var i = cookie.indexOf(" "+name+"=");
     if(i<0) i = cookie.indexOf(";"+name+"=");
@@ -34,4 +34,53 @@ function get_cookie(name) {
     var j = cookie.indexOf(";", i+1);
     if(j<0) j = cookie.length;
     return unescape(cookie.substring(i + name.length + 2, j));
+}
+
+function get_default_level() {
+    var level = get_cookie("toc_level");
+    if (level == "") { level = 3; }
+    return level;
+}
+
+function create_toc_control(level) {
+    var c = document.getElementById('toc_controls');
+    if(c) {
+        var txt = "<select id='toc_level' onChange='change_toc_level()'>";
+        txt += "<option value='1'";
+        if (level == 1) { txt += " selected"; }
+        txt += ">Less Detail</option>";
+        txt += "<option value='2'";
+        if (level == 2) { txt += " selected"; }
+        txt += ">Some Detail</option>";
+        txt += "<option value='3'";
+        if (level == 3) { txt += " selected"; }
+        txt += ">More Detail</option>";
+        txt += "</select>";
+        c.innerHTML = txt;
+    }
+}
+
+function change_toc_level() {
+    var field = document.getElementById('toc_level');
+    if(field) {
+        set_cookie("toc_level", field.value);
+        $("#toc").remove();
+        $("#toc_parent").append("<div id='toc'></div>");
+        generate_toc(field.value);
+    }
+}
+
+function generate_toc(level) {
+    var sstr = "h1";
+    for (var i=2; i<=level; i++) {
+        sstr += ",h"+i;
+    }
+    $("#toc").tocify({
+      context : "#technical_content",
+      showAndHide : false,
+      theme : "jqueryui",
+      ignoreSelector : ".title",
+      hashGenerator : wee_gen_id,
+      selectors : sstr
+      }).data("toc-tocify");
 }
