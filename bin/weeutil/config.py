b016dc182606226b19d360ed44e5f7ff2c68d16e
Tom Keffer <tkeffer@gmail.com>
2015-02-24 17:39:46 -0800

    Removed $Id tags from *.py files used by this branch

--- bin/weeutil/config.py
+++ bin/weeutil/config.py
@@ -1,11 +1,8 @@
 #
-#    Copyright (c) 2015 Tom Keffer <tkeffer@gmail.com>
+#    Copyright (c) 2009-2015 Tom Keffer <tkeffer@gmail.com>
 #
 #    See the file LICENSE.txt for your full rights.
 #
-#    $Id$
-#
-
 """Utilities used by the setup and configure programs"""
 
 import os
