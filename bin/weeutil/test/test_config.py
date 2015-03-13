87ece3d7e59ffe17f2be5b40600a76118441e70d
Tom Keffer <tkeffer@gmail.com>
2015-03-12 19:20:59 -0700

    Added tests for upgrading configuration file to V3.0

--- bin/weeutil/test/test_config.py
+++ bin/weeutil/test/test_config.py
@@ -25,14 +25,32 @@ class ConfigTest(unittest.TestCase):
         config20_dict.write(out_str)
 
         out_str.seek(0)
-        fd_expected = open('expected/weewx27.conf')
+        fd_expected = open('expected/weewx27_expected.conf')
         for expected in fd_expected:
             actual = out_str.readline()
             self.assertEqual(actual, expected)
         
-        # Now upgrade to V3.x:
+        out_str.close()
         
+    def test_upgrade_30(self):
         
+        # Start with the Version 2.7 weewx.conf file:
+        config27_dict = configobj.ConfigObj('weewx27.conf')  
+
+        # Upgrade to V3.0
+        old_database = weeutil.config.update_to_v3(config27_dict)
+        self.assertEqual(old_database, 'archive_sqlite')
+        
+        out_str = StringIO.StringIO()
+        config27_dict.write(out_str)
+
+        out_str.seek(0)
+        fd_expected = open('expected/weewx30_expected.conf')
+        for expected in fd_expected:
+            actual = out_str.readline()
+            self.assertEqual(actual, expected)
+        
+        out_str.close()
         
 if __name__ == '__main__':
     unittest.main()
