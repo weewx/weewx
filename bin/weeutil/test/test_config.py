aa96f56110418c28bcd4c92e7f9ed30e558a1ad5
Tom Keffer <tkeffer@gmail.com>
2015-03-29 07:42:54 -0700

    Updated test to reflect change in name to config_util

--- bin/weeutil/test/test_config.py
+++ bin/weeutil/test/test_config.py
@@ -9,7 +9,7 @@ import StringIO
 import unittest
 import configobj
 
-import weeutil.config
+import config_util
 
 x_str = """
         [section_a]
@@ -36,13 +36,13 @@ class ConfigTest(unittest.TestCase):
         
         xio = StringIO.StringIO(x_str)
         x_dict = configobj.ConfigObj(xio)
-        weeutil.config.reorder_sections(x_dict, 'section_c', 'section_b')
+        config_util.reorder_sections(x_dict, 'section_c', 'section_b')
         self.assertEqual("{'section_a': {'a': '1'}, 'section_c': {'c': '3'}, "
                          "'section_b': {'b': '2'}, 'section_d': {'d': '4'}}", str(x_dict))
         
         xio.seek(0)
         x_dict = configobj.ConfigObj(xio)
-        weeutil.config.reorder_sections(x_dict, 'section_c', 'section_b', after=True)
+        config_util.reorder_sections(x_dict, 'section_c', 'section_b', after=True)
         self.assertEqual("{'section_a': {'a': '1'}, 'section_b': {'b': '2'}, "
                          "'section_c': {'c': '3'}, 'section_d': {'d': '4'}}", str(x_dict))
         
@@ -50,7 +50,7 @@ class ConfigTest(unittest.TestCase):
         yio = StringIO.StringIO(y_str)
         x_dict = configobj.ConfigObj(xio)
         y_dict = configobj.ConfigObj(yio)
-        weeutil.config.conditional_merge(x_dict, y_dict)
+        config_util.conditional_merge(x_dict, y_dict)
         self.assertEqual("{'section_a': {'a': '1'}, 'section_b': {'b': '2'}, 'section_c': {'c': '3'}, "
                          "'section_d': {'d': '4'}, 'section_e': {'c': '15'}}", str(x_dict))
 
@@ -59,7 +59,7 @@ class ConfigTest(unittest.TestCase):
         yio = StringIO.StringIO(y_str)
         x_dict = configobj.ConfigObj(xio)
         y_dict = configobj.ConfigObj(yio)
-        weeutil.config.remove_and_prune(x_dict, y_dict)
+        config_util.remove_and_prune(x_dict, y_dict)
         self.assertEqual("{'section_c': {'c': '3'}, 'section_d': {'d': '4'}}", str(x_dict))
 
     def test_upgrade_v27(self):
@@ -68,7 +68,7 @@ class ConfigTest(unittest.TestCase):
         config_dict = configobj.ConfigObj('weewx20.conf')
         
         # Upgrade the V2.0 configuration dictionary to V2.7:
-        weeutil.config.update_to_v27(config_dict)
+        config_util.update_to_v27(config_dict)
         
         # Write it out to a StringIO, then start checking it against the expected
         out_str = StringIO.StringIO()
@@ -92,7 +92,7 @@ class ConfigTest(unittest.TestCase):
         config_dict = configobj.ConfigObj('weewx27.conf')  
 
         # Upgrade to V3.0
-        weeutil.config.update_to_v30(config_dict)
+        config_util.update_to_v30(config_dict)
         
         # Write it out to a StringIO, then start checking it against the expected
         out_str = StringIO.StringIO()
@@ -119,8 +119,8 @@ class ConfigTest(unittest.TestCase):
         template = configobj.ConfigObj('weewx31.conf')
         
         # First update, then merge:
-        weeutil.config.update_config(config_dict)
-        weeutil.config.merge_config(config_dict, template)
+        config_util.update_config(config_dict)
+        config_util.merge_config(config_dict, template)
         
         # Write it out to a StringIO, then start checking it against the expected
         out_str = StringIO.StringIO()
