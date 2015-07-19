6476ec4e14503b8aeaebbfaab99650c604e74116
Tom Keffer <tkeffer@gmail.com>
2015-07-19 06:54:56 -0700

    Now sets WEEWX_ROOT to a normative path (avoids trailing slash)

--- setup.py
+++ setup.py
@@ -193,7 +193,7 @@ class weewx_install_data(install_data):
             weecfg.modify_config(config_dict, stn_info, DEBUG)
 
         # Set the WEEWX_ROOT
-        config_dict['WEEWX_ROOT'] = install_dir
+        config_dict['WEEWX_ROOT'] = os.path.normpath(install_dir)
         # Finally, reorder it to the canonical form
         weecfg.reorder_to_ref(config_dict)
     
