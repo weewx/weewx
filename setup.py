fcdf891c594f4264148f6c6963c8a8e222ea971e
Matthew Wall <mwall@users.sourceforge.net>
2017-01-08 18:19:18 -0500

    incorporate windows mods from dcapslock

--- setup.py
+++ setup.py
@@ -441,7 +441,7 @@ if __name__ == "__main__":
                     'weeutil',
                     'weewx',
                     'weewx.drivers'],
-          py_modules=['daemon'],
+          py_modules=['daemon', 'syslog'],
           scripts=['bin/wee_config',
                    'bin/wee_database',
                    'bin/wee_debug',
