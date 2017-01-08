adc8c56d9dcf4f289869277012866a0d1607911f
Matthew Wall <mwall@users.sourceforge.net>
2017-01-08 10:53:45 -0500

    use 3.7 to indicate unreleased.  when there is a third number then it has been released.

--- bin/weewx/__init__.py
+++ bin/weewx/__init__.py
@@ -6,7 +6,7 @@
 """Package weewx, containing modules specific to the weewx runtime engine."""
 import time
 
-__version__="3.7.1000"
+__version__="3.7"
 
 # Holds the program launch time in unix epoch seconds:
 # Useful for calculating 'uptime.'
