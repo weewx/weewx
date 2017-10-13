172e44b22c60516b3df67831d7be147af8ddca2d
Tom Keffer <tkeffer@gmail.com>
2017-10-13 08:04:00 -0700

    V3.8.0 release candidate a0

--- bin/weewx/__init__.py
+++ bin/weewx/__init__.py
@@ -6,7 +6,7 @@
 """Package weewx, containing modules specific to the weewx runtime engine."""
 import time
 
-__version__="3.7.1"
+__version__="3.8.0a1"
 
 # Holds the program launch time in unix epoch seconds:
 # Useful for calculating 'uptime.'
