0616961fb11bebbddea8ec2a524b864b1b60accf
Tom Keffer <tkeffer@gmail.com>
2018-12-08 10:44:31 -0800

    Ported almanac doctests to Python 3

--- bin/weewx/almanac.py
+++ bin/weewx/almanac.py
@@ -8,6 +8,7 @@
 This module can optionally use PyEphem, which offers high quality
 astronomical calculations. See http://rhodesmill.org/pyephem. """
 
+from __future__ import print_function
 import time
 import sys
 import math
@@ -22,7 +23,7 @@ try:
 except ImportError:
     import weeutil.Sun
 
-# NB: Have Almanac inherit from 'object'. However, this will cause 
+# NB: Have Almanac inherit from 'object'. However, this will cause
 # an 'autocall' bug in Cheetah versions before 2.1.
 class Almanac(object):
     """Almanac data.
@@ -50,122 +51,126 @@ class Almanac(object):
     
     EXAMPLES:
     
+    These examples require pyephem to be installed.
+    >>> if "ephem" not in sys.modules:
+    ...   raise KeyboardInterrupt("Almanac examples require 'pyephem'")
+
     These examples are designed to work in the Pacific timezone
     >>> import os
     >>> os.environ['TZ'] = 'America/Los_Angeles'
     >>> from weeutil.weeutil import timestamp_to_string, timestamp_to_gmtime
     >>> t = 1238180400
-    >>> print timestamp_to_string(t)
+    >>> print(timestamp_to_string(t))
     2009-03-27 12:00:00 PDT (1238180400)
     
     Test conversions to Dublin Julian Days
     >>> t_djd = timestamp_to_djd(t)
-    >>> print "%.5f" % t_djd
+    >>> print("%.5f" % t_djd)
     39898.29167
     
     Test the conversion back
-    >>> print "%.0f" % djd_to_timestamp(t_djd)
+    >>> print("%.0f" % djd_to_timestamp(t_djd))
     1238180400
     
     >>> almanac = Almanac(t, 46.0, -122.0)
     
     Test backwards compatibility with attribute 'moon_fullness':
-    >>> print "Fullness of the moon (rounded) is %.2f%% [%s]" % (almanac._moon_fullness, almanac.moon_phase)
+    >>> print("Fullness of the moon (rounded) is %.2f%% [%s]" % (almanac._moon_fullness, almanac.moon_phase))
     Fullness of the moon (rounded) is 5.00% [waxing crescent (increasing to full)]
     
     Now get a more precise result for fullness of the moon:
-    >>> print "Fullness of the moon (more precise) is %.2f%%" % almanac.moon.moon_fullness
+    >>> print("Fullness of the moon (more precise) is %.2f%%" % almanac.moon.moon_fullness)
     Fullness of the moon (more precise) is 1.70%
 
     Test backwards compatibility with attributes 'sunrise' and 'sunset'
-    >>> print "Sunrise, sunset:", almanac.sunrise, almanac.sunset
+    >>> print("Sunrise, sunset:", almanac.sunrise, almanac.sunset)
     Sunrise, sunset: 06:56 19:30
 
     Get sunrise, sun transit, and sunset using the new 'ephem' syntax:
-    >>> print "Sunrise, sun transit, sunset:", almanac.sun.rise, almanac.sun.transit, almanac.sun.set
+    >>> print("Sunrise, sun transit, sunset:", almanac.sun.rise, almanac.sun.transit, almanac.sun.set)
     Sunrise, sun transit, sunset: 06:56 13:13 19:30
     
     Do the same with the moon:
-    >>> print "Moon rise, transit, set:", almanac.moon.rise, almanac.moon.transit, almanac.moon.set
+    >>> print("Moon rise, transit, set:", almanac.moon.rise, almanac.moon.transit, almanac.moon.set)
     Moon rise, transit, set: 06:59 14:01 21:20
     
     And Mars
-    >>> print "Mars rise, transit, set:", almanac.mars.rise, almanac.mars.transit, almanac.moon.set
+    >>> print("Mars rise, transit, set:", almanac.mars.rise, almanac.mars.transit, almanac.moon.set)
     Mars rise, transit, set: 06:08 11:34 21:20
     
     Finally, try a star
-    >>> print "Rigel rise, transit, set:", almanac.rigel.rise, almanac.rigel.transit, almanac.rigel.set 
+    >>> print("Rigel rise, transit, set:", almanac.rigel.rise, almanac.rigel.transit, almanac.rigel.set)
     Rigel rise, transit, set: 12:32 18:00 23:28
 
     Exercise sidereal time
-    >>> print almanac.sidereal_time
-    348.340037436
+    >>> print("%.4f" % almanac.sidereal_time)
+    348.3400
 
     Exercise equinox, solstice routines
-    >>> print almanac.next_vernal_equinox
+    >>> print(almanac.next_vernal_equinox)
     20-Mar-2010 10:32
-    >>> print almanac.next_autumnal_equinox
+    >>> print(almanac.next_autumnal_equinox)
     22-Sep-2009 14:18
-    >>> print almanac.next_summer_solstice
+    >>> print(almanac.next_summer_solstice)
     20-Jun-2009 22:45
-    >>> print almanac.previous_winter_solstice
+    >>> print(almanac.previous_winter_solstice)
     21-Dec-2008 04:03
-    >>> print almanac.next_winter_solstice
+    >>> print(almanac.next_winter_solstice)
     21-Dec-2009 09:46
     
     Exercise moon state routines
-    >>> print almanac.next_full_moon
+    >>> print(almanac.next_full_moon)
     09-Apr-2009 07:55
-    >>> print almanac.next_new_moon
+    >>> print(almanac.next_new_moon)
     24-Apr-2009 20:22
-    >>> print almanac.next_first_quarter_moon
+    >>> print(almanac.next_first_quarter_moon)
     02-Apr-2009 07:33
     
     Now location of the sun and moon
-    >>> print "Solar azimuth, altitude = (%.2f, %.2f)" % (almanac.sun.az, almanac.sun.alt)
+    >>> print("Solar azimuth, altitude = (%.2f, %.2f)" % (almanac.sun.az, almanac.sun.alt))
     Solar azimuth, altitude = (154.14, 44.02)
-    >>> print "Moon azimuth, altitude = (%.2f, %.2f)" % (almanac.moon.az, almanac.moon.alt)
+    >>> print("Moon azimuth, altitude = (%.2f, %.2f)" % (almanac.moon.az, almanac.moon.alt))
     Moon azimuth, altitude = (133.55, 47.89)
     
     Try a time and location where the sun is always up
     >>> t = 1371044003
-    >>> print timestamp_to_string(t)
+    >>> print(timestamp_to_string(t))
     2013-06-12 06:33:23 PDT (1371044003)
     >>> almanac = Almanac(t, 64.0, 0.0)
-    >>> print almanac(horizon=-6).sun(use_center=1).rise
+    >>> print(almanac(horizon=-6).sun(use_center=1).rise)
        N/A
 
     Try the pyephem "Naval Observatory" example.
     >>> t = 1252256400
-    >>> print timestamp_to_gmtime(t)
+    >>> print(timestamp_to_gmtime(t))
     2009-09-06 17:00:00 UTC (1252256400)
     >>> atlanta = Almanac(t, 33.8, -84.4, pressure=0, horizon=-34.0/60.0)
     >>> # Print it in GMT, so it can easily be compared to the example:
-    >>> print timestamp_to_gmtime(atlanta.sun.previous_rising.raw) 
+    >>> print(timestamp_to_gmtime(atlanta.sun.previous_rising.raw))
     2009-09-06 11:14:56 UTC (1252235696)
-    >>> print timestamp_to_gmtime(atlanta.moon.next_setting.raw)
+    >>> print(timestamp_to_gmtime(atlanta.moon.next_setting.raw))
     2009-09-07 14:05:29 UTC (1252332329)
     
     Now try the civil twilight examples:
-    >>> print timestamp_to_gmtime(atlanta(horizon=-6).sun(use_center=1).previous_rising.raw)
+    >>> print(timestamp_to_gmtime(atlanta(horizon=-6).sun(use_center=1).previous_rising.raw))
     2009-09-06 10:49:40 UTC (1252234180)
-    >>> print timestamp_to_gmtime(atlanta(horizon=-6).sun(use_center=1).next_setting.raw)
+    >>> print(timestamp_to_gmtime(atlanta(horizon=-6).sun(use_center=1).next_setting.raw))
     2009-09-07 00:21:22 UTC (1252282882)
 
     Try sun rise again, to make sure the horizon value cleared:
-    >>> print timestamp_to_gmtime(atlanta.sun.previous_rising.raw) 
+    >>> print(timestamp_to_gmtime(atlanta.sun.previous_rising.raw))
     2009-09-06 11:14:56 UTC (1252235696)
 
     Try an attribute that does not explicitly appear in the class Almanac
-    >>> print "%.3f" % almanac.mars.sun_distance
+    >>> print("%.3f" % almanac.mars.sun_distance)
     1.494
 
     Try a specialized attribute for Jupiter
-    >>> print almanac.jupiter.cmlI
+    >>> print(almanac.jupiter.cmlI)
     191:16:58.0
 
     Should fail if applied to a different body
-    >>> print almanac.venus.cmlI
+    >>> print(almanac.venus.cmlI)
     Traceback (most recent call last):
         ...
     AttributeError: 'Venus' object has no attribute 'cmlI'
