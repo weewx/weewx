2c95c23a223945c50243e2b7fa2029e9dcc3a0ff
Matthew Wall <mwall@users.sourceforge.net>
2019-02-01 05:15:11 -0500

    use localStorage instead of cookies

--- skins/Seasons/seasons.js
+++ skins/Seasons/seasons.js
@@ -4,11 +4,11 @@
  */
 
 var cookie_prefix = "weewx.seasons.";
-var year_type = get_cookie('year_type', 'year');
+var year_type = get_state('year_type', 'year');
 
 function setup(widgets) {
     // set the state of the history widget
-    var id = get_cookie('history', 'day');
+    var id = get_state('history', 'day');
     choose_history(id);
     // if we got a list of widget names, then use it.  otherwise, query the doc
     // for every object with an id of *_widget, and use that as the name list.
@@ -28,7 +28,7 @@ function setup(widgets) {
     }
     // now set the toggle state for each widget based on what the cookies say
     for (i = 0; i < widgets.length; i++) {
-        var state = get_cookie(widgets[i] + '.state', 'expanded');
+        var state = get_state(widgets[i] + '.state', 'expanded');
         toggle_widget(widgets[i], state);
     }
 }
@@ -51,7 +51,7 @@ function toggle_rainyear() {
     } else {
         year_type = 'year';
     }
-    set_cookie('year_type', year_type);
+    set_state('year_type', year_type);
     var id = get_active_div('history', ['day', 'week', 'month', 'year'], 'day');
     choose_rainyear(id);
 }
@@ -68,7 +68,7 @@ function toggle_widget(id, state) {
                 id_elements.childNodes[i].style.display = (state === 'expanded') ? 'block' : 'none';
             }
         }
-        set_cookie(id + '.state', state);
+        set_state(id + '.state', state);
     }
 }
 
@@ -95,7 +95,7 @@ function choose_div(group, selected_id, all_ids) {
             element.style.display = (all_ids[i] === selected_id) ? 'block' : 'none';
         }
     }
-    set_cookie(group, selected_id);
+    set_state(group, selected_id);
 }
 
 /* if cookies are disabled, then we must look at page to get state */
@@ -110,24 +110,39 @@ function get_active_div(group, all_ids, default_value) {
     return id;
 }
 
+function set_state(name, value, dur) {
+    var full_name = cookie_prefix + name;
+/*    set_cookie(full_name, value, dur); */
+    window.localStorage.setItem(full_name, value);
+}
+
+function get_state(name, default_value) {
+    var full_name = cookie_prefix + name;
+/*    return get_cookie(name, default_value); */
+    var value = window.localStorage.getItem(full_name);
+    if (value === undefined || value == null) {
+        value = default_value;
+    }
+    return value
+}
+
 function set_cookie(name, value, dur) {
     if (!dur) dur = 30;
     var today = new Date();
     var expire = new Date();
     expire.setTime(today.getTime() + 24 * 3600000 * dur);
-    document.cookie = cookie_prefix + name + "=" + encodeURI(value) + ";expires=" + expire.toUTCString();
+    document.cookie = name + "=" + encodeURI(value) + ";expires=" + expire.toUTCString();
 }
 
 function get_cookie(name, default_value) {
     if (name === "") return default_value;
-    var full_name = cookie_prefix + name;
     var cookie = " " + document.cookie;
-    var i = cookie.indexOf(" " + full_name + "=");
-    if (i < 0) i = cookie.indexOf(";" + full_name + "=");
+    var i = cookie.indexOf(" " + name + "=");
+    if (i < 0) i = cookie.indexOf(";" + name + "=");
     if (i < 0) return default_value;
     var j = cookie.indexOf(";", i + 1);
     if (j < 0) j = cookie.length;
-    return unescape(cookie.substring(i + full_name.length + 2, j));
+    return unescape(cookie.substring(i + name.length + 2, j));
 }
 
 function get_parameter(name) {
