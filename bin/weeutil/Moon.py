c696e468efce806214cd560e1014cb50d382c68c
Tom Keffer <tkeffer@gmail.com>
2019-02-27 10:59:40 -0800

    Improved accuracy of the calculation of Moon phases. Revisits issue #342.

--- bin/weeutil/Moon.py
+++ bin/weeutil/Moon.py
@@ -1,9 +1,9 @@
 #
-#    Copyright (c) 2009-2018 Tom Keffer <tkeffer@gmail.com>
+#    Copyright (c) 2009-2019 Tom Keffer <tkeffer@gmail.com>
 #
 #    See the file LICENSE.txt for your full rights.
 #
-"""Determine the phase of the moon phase given a date."""
+"""Given a date, determine the phase of the moon."""
 
 import time
 import math
@@ -17,8 +17,8 @@ moon_phases = ["new (totally dark)",
                "in its last quarter (decreasing from full)",
                "waning crescent (decreasing from full)"]
 
-# First new moon of 2018: 16-Jan-2018 at 1017UTC
-new_moon_2018 = 1516097820
+# First new moon of 2018: 17-Jan-2018 at 02:17 UTC
+new_moon_2018 = 1516155420
 
 
 def moon_phase(year, month, day, hour=12):
@@ -32,10 +32,14 @@ def moon_phase(year, month, day, hour=12):
     # Convert to UTC
     time_ts = time.mktime((year, month, day, hour, 0, 0, 0, 0, -1))
 
+    return moon_phase_ts(time_ts)
+
+
+def moon_phase_ts(time_ts):
     # How many days since the first moon of 2018
-    delta_days = (time_ts - new_moon_2018) / 86400
+    delta_days = (time_ts - new_moon_2018) / 86400.0
     # Number of lunations
-    lunations = delta_days / 29.530589
+    lunations = delta_days / 29.530588
 
     # The fraction of the lunar cycle
     position = float(lunations) % 1.0
