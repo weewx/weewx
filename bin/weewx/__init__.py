f62d0cb9571b651db46931e10d7641770fc549cf
Tom Keffer <tkeffer@gmail.com>
2019-02-28 06:08:05 -0800

    Bumped version to 3.9.2a1

--- bin/weewx/__init__.py
+++ bin/weewx/__init__.py
@@ -6,7 +6,7 @@
 """Package weewx, containing modules specific to the weewx runtime engine."""
 import time
 
-__version__="3.9.1"
+__version__="3.9.2a1"
 
 # Holds the program launch time in unix epoch seconds:
 # Useful for calculating 'uptime.'
