e0296e1f9bbcc2e3554fdb5a68e041027c7bf602
Tom Keffer <tkeffer@gmail.com>
2019-03-05 07:29:43 -0800

    Minor cleanup

--- bin/weewx/tests/test_restx.py
+++ bin/weewx/tests/test_restx.py
@@ -9,9 +9,11 @@ import time
 import unittest
 
 try:
-    import mock
-except ImportError:
+    # Python 3 --- mock is included in unittest
     from unittest import mock
+except ImportError:
+    # Python 2 --- must have mock installed
+    import mock
 
 from six.moves import queue
 from six.moves import urllib
@@ -21,42 +23,36 @@ import weewx.restx
 
 
 class MatchRequest(object):
-    """Allows equality testing against Request objects"""
+    """Allows equality testing between Request objects"""
 
     def __init__(self, url, user_agent):
         self.url = url
         self.user_agent = user_agent
 
-    def __eq__(self, other):
-        other_split = urllib.parse.urlsplit(other.get_full_url())
+    def __eq__(self, req):
+        """Check for equality between myself and a Request object"""
+
+        other_split = urllib.parse.urlsplit(req.get_full_url())
         other_query = urllib.parse.parse_qs(other_split.query)
         my_split = urllib.parse.urlsplit(self.url)
         my_query = urllib.parse.parse_qs(my_split.query)
 
         # In what follows, we could just return 'False', but raising the AssertionError directly
-        # gives a more useful error message.
+        # gives more useful error messages.
 
         if other_split.hostname != my_split.hostname:
             raise AssertionError(
                 "Mismatched hostnames: \nActual:'%s'\nExpect:'%s' " % (other_split.hostname, my_split.hostname))
         if other_query != my_query:
             raise AssertionError("Mismatched queries: \nActual:'%s'\nExpect:'%s' " % (other_query, my_query))
-        if other.headers.get('User-agent') != self.user_agent:
+        if req.headers.get('User-agent') != self.user_agent:
             raise AssertionError("Mismatched user-agent: \nActual:'%s'\nExpect:'%s'"
-                                 % (other.headers.get('User-agent'), self.user_agent))
+                                 % (req.headers.get('User-agent'), self.user_agent))
         return True
 
 
-def get_patcher(response_body=[]):
-    patcher = mock.patch('weewx.restx.urllib.request.urlopen')
-    mock_urlopen = patcher.start()
-    mock_urlopen.return_value = mock.MagicMock(name='return value')
-    mock_urlopen.return_value.code = 200
-    mock_urlopen.return_value.__iter__.return_value = iter(response_body)
-    return mock_urlopen
-
-
 def get_record():
+    """Get a record that is to be posted to the RESTful service."""
     ts = time.mktime(time.strptime("2018-03-22", "%Y-%m-%d"))
     record = {'dateTime': ts,
               'usUnits': weewx.US,
@@ -75,27 +71,42 @@ class TestAmbient(unittest.TestCase):
     password = 'somepassword'
     server_url = 'http://www.testserver.com/testapi'
 
+    def get_patcher(self, code=200, response_body=[]):
+        """Get a patch object for a post to urllib.request.urlopen
+
+        code: The response code that should be returned by the mock urlopen
+
+        response_body: The response body that should be returned by the mock urlopen
+        """
+        # Mock up the urlopen
+        patcher = mock.patch('weewx.restx.urllib.request.urlopen')
+        mock_urlopen = patcher.start()
+        # Set up its return value
+        mock_urlopen.return_value = mock.MagicMock(name='urlopen return value')
+        mock_urlopen.return_value.code = code
+        mock_urlopen.return_value.__iter__.return_value = iter(response_body)
+        # This will insure that patcher.stop() will get called
+        self.addCleanup(patcher.stop)
+        return mock_urlopen
+
     def test_request(self):
         """Test that we are forming the right Request object"""
 
-        mock_urlopen = get_patcher()
-        try:
-            q = queue.Queue()
-            obj = weewx.restx.AmbientThread(q,
-                                            manager_dict=None,
-                                            station=TestAmbient.station,
-                                            password=TestAmbient.password,
-                                            server_url=TestAmbient.server_url,
-                                            )
-            record = get_record()
-            q.put(record)
-            q.put(None)
-            obj.run()
+        mock_urlopen = self.get_patcher()
+        q = queue.Queue()
+        obj = weewx.restx.AmbientThread(q,
+                                        manager_dict=None,
+                                        station=TestAmbient.station,
+                                        password=TestAmbient.password,
+                                        server_url=TestAmbient.server_url,
+                                        )
+        record = get_record()
+        q.put(record)
+        q.put(None)
+        obj.run()
 
-            matcher = TestAmbient.get_matcher(TestAmbient.server_url, TestAmbient.station, TestAmbient.password)
-            mock_urlopen.assert_called_once_with(matcher, data=None, timeout=10)
-        finally:
-            mock_urlopen.stop()
+        matcher = TestAmbient.get_matcher(TestAmbient.server_url, TestAmbient.station, TestAmbient.password)
+        mock_urlopen.assert_called_once_with(matcher, data=None, timeout=10)
 
     @staticmethod
     def get_matcher(server_url, station, password):
