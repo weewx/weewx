weewx/weewx

commit 0214532037e9cf85bdcd009e2e9f74edfa05351c

An observation type known to the system, but not in a record, will now return a proper ValueTuple, rather than UnknownType.

bin/weewx/units.py
@@ -1286,29 +1286,33 @@ def to_std_system(datadict, unit_system):
         _datadict_target['usUnits'] = unit_system
         return _datadict_target
 
+
 def as_value_tuple(record_dict, obs_type):
-    """Look up an observation type in a record dictionary, returning the result
-    as a ValueTuple. If not found, an object of type UnknownType will be returned."""
+    """Look up an observation type in a record, returning the result
+    as a ValueTuple. If the observation type is not recognized, an object of
+    type UnknownType will be returned."""
 
     # Is the record None?
     if record_dict is None:
-        # Yes. The record was probably not in the database. Signal a value of None
-        # and, arbitrarily, pick the US unit system:
+        # Yes. Signal a value of None and, arbitrarily, pick the US unit system:
         val = None
         std_unit_system = weewx.US
-    elif obs_type not in record_dict:
-        # There is a record, but the observation type is not in it
-        return UnknownType(obs_type)
     else:
-        # There is a record, and the observation type is in it.
-        val = record_dict[obs_type]
+        # There is a record. Get the value, and the unit system.
+        val = record_dict.get(obs_type)
         std_unit_system = record_dict['usUnits']
 
     # Given this standard unit system, what is the unit type of this
-    # particular observation type?
+    # particular observation type? If the observation type is not recognized,
+    # a unit_type of None will be returned
     (unit_type, unit_group) = StdUnitConverters[std_unit_system].getTargetUnit(obs_type)
+
+    if unit_type is None:
+        return UnknownType(obs_type)
+
     # Form the value-tuple and return it:
     return ValueTuple(val, unit_type, unit_group)
+
 
 if __name__ == "__main__":
     

commit 7083dd2ea0fd76ac04996139b3bf48728ece8050

Fixed problem introduced by commit https://github.com/weewx/weewx/commit/0214532037e9cf85bdcd009e2e9f74edfa05351c

bin/weewx/tags.py
@@ -401,24 +401,31 @@ class CurrentObj(object):
         if obs_type in ['__call__', 'has_key']:
             raise AttributeError
 
-        # If we are not specifying a data binding, and we have a current record with the right
+        # If no data binding has been specified, and we have a current record with the right
         # timestamp at hand, we don't have to hit the database.
-        if not self.data_binding and self.record and obs_type in self.record and self.record['dateTime'] == self.current_time:
+        if not self.data_binding and self.record and obs_type in self.record \
+                and self.record['dateTime'] == self.current_time:
+            # Use the record given to us to form a ValueTuple
             vt = weewx.units.as_value_tuple(self.record, obs_type)
         else:
-            # No. We have to retrieve the record from the database
+            # A binding has been specified, or we don't have a record, or the observation type
+            # is not in the record, or the timestamp is wrong.
             try:
-                # Get the appropriate database manager ...
+                # Get the appropriate database manager
                 db_manager = self.db_lookup(self.data_binding)
             except weewx.UnknownBinding:
+                # Don't recognize the binding.
                 vt = weewx.units.UnknownType(self.data_binding)
             else:
-                # ... get the current record from it ...  
-                record  = db_manager.getRecord(self.current_time, max_delta=self.max_delta)
-                # ... form a ValueTuple ...
-                vt = weewx.units.as_value_tuple(record, obs_type)
-            # ... and then finally, return a ValueHelper
+                # Does the type exist in the database? If not, it's an unknown type
+                if obs_type not in db_manager.sqlkeys:
+                    vt = weewx.units.UnknownType(obs_type)
+                else:
+                    # Get the record from the database
+                    record = db_manager.getRecord(self.current_time, max_delta=self.max_delta)
+                    vt = weewx.units.as_value_tuple(record, obs_type)
 
+        # Finally, return a ValueHelper
         return weewx.units.ValueHelper(vt, 'current',
                                        self.formatter,
                                        self.converter)

bin/weewx/units.py
@@ -1313,9 +1313,6 @@ def as_value_tuple(record_dict, obs_type):
     # a unit_type of None will be returned
     (unit_type, unit_group) = StdUnitConverters[std_unit_system].getTargetUnit(obs_type)
 
-    if unit_type is None:
-        return UnknownType(obs_type)
-
     # Form the value-tuple and return it:
     return ValueTuple(val, unit_type, unit_group)
 

bin/weewx/tests/expected/StandardTest/index.html
@@ -88,7 +88,7 @@
       <td>45.1&#176;F</td>
     </tr>
     <tr>
-      <td>Outside Temperature with nonsense binding 'foo_binding'</td>
+      <td>Outside Temperature with nonsense binding $current($data_binding='foo_binding').outTemp</td>
       <td>?'foo_binding'?</td>
     </tr>
     <tr>
@@ -169,24 +169,24 @@
     </tr>
     <tr>
       <td>Test tag "exists" for an existent type: $current.outTemp.exists</td>
-      <td>PASS</td>
-    </tr> 
+      <td>TRUE</td>
+    </tr>
     <tr>
       <td>Test tag "exists" for a nonsense type: $current.nonsense.exists</td>
-      <td>PASS</td>
-    </tr> 
+      <td>FALSE</td>
+    </tr>
     <tr>
       <td>Test tag "has_data" for an existing type with data: $current.outTemp.has_data</td>
-      <td>PASS</td>
+      <td>TRUE</td>
     </tr>
     <tr>
       <td>Test tag "has_data" for an existing type without data: $current.hail.has_data</td>
-      <td>PASS</td>
+      <td>FALSE</td>
     </tr>
     <tr>
       <td>Test tag "has_data" for a nonsense type: $current.nonsense.has_data</td>
-      <td>PASS</td>
-    </tr> 
+      <td>FALSE</td>
+    </tr>
     <tr>
       <td>Test for a bad observation type on a $current tag: $current.foobar</td>
       <td>?'foobar'?</td>
@@ -435,20 +435,20 @@
       <td>107&#176;</td>
     </tr>
     <tr>
-      <td>Test tag "has_data" with nonsense type</td>
-      <td>PASS</td>
+      <td>Test tag "has_data" with nonsense type $day.nonsense.has_data</td>
+      <td>FALSE</td>
     </tr>
     <tr>
-      <td>Test tag "exists" with an existing type that has no data</td>
-      <td>PASS</td>
+      <td>Test tag "exists" with an existing type that has no data $day.heatindex.exists</td>
+      <td>TRUE</td>
     </tr>
     <tr>
-      <td>Test tag "has_data" with existent type that has no data</td>
-      <td>PASS</td>
+      <td>Test tag "has_data" with existent type that has no data $day.heatindex.has_data</td>
+      <td>FALSE</td>
     </tr>
     <tr>
-      <td>Test tag "has_data" with existent type that has data</td>
-      <td>PASS</td>
+      <td>Test tag "has_data" with existent type that has data $day.outTemp.has_data</td>
+      <td>TRUE</td>
     </tr>
       <tr>
       <td>Test for a bad observation type on a $day tag: $day.foobar.min</td>

bin/weewx/tests/expected/StandardTest/metric/index.html
@@ -88,7 +88,7 @@
       <td>7.3&#176;C</td>
     </tr>
     <tr>
-      <td>Outside Temperature with nonsense binding 'foo_binding'</td>
+      <td>Outside Temperature with nonsense binding $current($data_binding='foo_binding').outTemp</td>
       <td>?'foo_binding'?</td>
     </tr>
     <tr>
@@ -169,24 +169,24 @@
     </tr>
     <tr>
       <td>Test tag "exists" for an existent type: $current.outTemp.exists</td>
-      <td>PASS</td>
-    </tr> 
+      <td>TRUE</td>
+    </tr>
     <tr>
       <td>Test tag "exists" for a nonsense type: $current.nonsense.exists</td>
-      <td>PASS</td>
-    </tr> 
+      <td>FALSE</td>
+    </tr>
     <tr>
       <td>Test tag "has_data" for an existing type with data: $current.outTemp.has_data</td>
-      <td>PASS</td>
+      <td>TRUE</td>
     </tr>
     <tr>
       <td>Test tag "has_data" for an existing type without data: $current.hail.has_data</td>
-      <td>PASS</td>
+      <td>FALSE</td>
     </tr>
     <tr>
       <td>Test tag "has_data" for a nonsense type: $current.nonsense.has_data</td>
-      <td>PASS</td>
-    </tr> 
+      <td>FALSE</td>
+    </tr>
     <tr>
       <td>Test for a bad observation type on a $current tag: $current.foobar</td>
       <td>?'foobar'?</td>
@@ -435,20 +435,20 @@
       <td>107&#176;</td>
     </tr>
     <tr>
-      <td>Test tag "has_data" with nonsense type</td>
-      <td>PASS</td>
+      <td>Test tag "has_data" with nonsense type $day.nonsense.has_data</td>
+      <td>FALSE</td>
     </tr>
     <tr>
-      <td>Test tag "exists" with an existing type that has no data</td>
-      <td>PASS</td>
+      <td>Test tag "exists" with an existing type that has no data $day.heatindex.exists</td>
+      <td>TRUE</td>
     </tr>
     <tr>
-      <td>Test tag "has_data" with existent type that has no data</td>
-      <td>PASS</td>
+      <td>Test tag "has_data" with existent type that has no data $day.heatindex.has_data</td>
+      <td>FALSE</td>
     </tr>
     <tr>
-      <td>Test tag "has_data" with existent type that has data</td>
-      <td>PASS</td>
+      <td>Test tag "has_data" with existent type that has data $day.outTemp.has_data</td>
+      <td>TRUE</td>
     </tr>
       <tr>
       <td>Test for a bad observation type on a $day tag: $day.foobar.min</td>

bin/weewx/tests/test_skins/StandardTest/index.html.tmpl
@@ -1,4 +1,11 @@
 #errorCatcher Echo
+#def emit_boolean(val)
+  #if val
+TRUE#slurp
+  #else
+FALSE#slurp
+  #end if
+#end def
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -92,7 +99,7 @@
       <td>$current($data_binding='alt_binding').outTemp</td>
     </tr>
     <tr>
-      <td>Outside Temperature with nonsense binding 'foo_binding'</td>
+      <td>Outside Temperature with nonsense binding \$current($data_binding='foo_binding').outTemp</td>
       <td>$current($data_binding='foo_binding').outTemp</td>
     </tr>
     <tr>
@@ -175,44 +182,24 @@
     </tr>
     <tr>
       <td>Test tag "exists" for an existent type: \$current.outTemp.exists</td>
-    #if $current.outTemp.exists
-      <td>PASS</td>
-    #else
-      <td>FAIL</td>
-    #end if
-    </tr> 
+      <td>$emit_boolean($current.outTemp.exists)</td>
+    </tr>
     <tr>
       <td>Test tag "exists" for a nonsense type: \$current.nonsense.exists</td>
-    #if $current.nonsense.exists
-      <td>FAIL</td>
-    #else
-      <td>PASS</td>
-    #end if
-    </tr> 
+      <td>$emit_boolean($current.nonsense.exists)</td>
+    </tr>
     <tr>
       <td>Test tag "has_data" for an existing type with data: \$current.outTemp.has_data</td>
-    #if $current.outTemp.has_data
-      <td>PASS</td>
-    #else
-      <td>FAIL</td>
-    #end if
+      <td>$emit_boolean($current.outTemp.has_data)</td>
     </tr>
     <tr>
       <td>Test tag "has_data" for an existing type without data: \$current.hail.has_data</td>
-    #if $current.hail.has_data
-      <td>FAIL</td>
-    #else
-      <td>PASS</td>
-    #end if
+      <td>$emit_boolean($current.hail.has_data)</td>
     </tr>
     <tr>
       <td>Test tag "has_data" for a nonsense type: \$current.nonsense.has_data</td>
-    #if $current.nonsense.has_data
-      <td>FAIL</td>
-    #else
-      <td>PASS</td>
-    #end if
-    </tr> 
+      <td>$emit_boolean($current.nonsense.has_data)</td>
+    </tr>
     <tr>
       <td>Test for a bad observation type on a \$current tag: \$current.foobar</td>
       <td>$current.foobar</td>
@@ -448,36 +435,20 @@
       <td>$day.wind.vecdir</td>
     </tr>
     <tr>
-      <td>Test tag "has_data" with nonsense type</td>
-      #if $day.nonsense.has_data
-      <td>FAILS</td>
-      #else
-      <td>PASS</td>
-      #end if
+      <td>Test tag "has_data" with nonsense type \$day.nonsense.has_data</td>
+      <td>$emit_boolean($day.nonsense.has_data)</td>
     </tr>
     <tr>
-      <td>Test tag "exists" with an existing type that has no data</td>
-      #if $day.heatindex.exists
-      <td>PASS</td>
-      #else
-      <td>FAIL</td>
-      #end if
+      <td>Test tag "exists" with an existing type that has no data \$day.heatindex.exists</td>
+      <td>$emit_boolean($day.heatindex.exists)</td>
     </tr>
     <tr>
-      <td>Test tag "has_data" with existent type that has no data</td>
-      #if $day.heatindex.has_data
-      <td>FAILS</td>
-      #else
-      <td>PASS</td>
-      #end if
+      <td>Test tag "has_data" with existent type that has no data \$day.heatindex.has_data</td>
+      <td>$emit_boolean($day.heatindex.has_data)</td>
     </tr>
     <tr>
-      <td>Test tag "has_data" with existent type that has data</td>
-      #if $day.outTemp.has_data
-      <td>PASS</td>
-      #else
-      <td>FAIL</td>
-      #end if
+      <td>Test tag "has_data" with existent type that has data \$day.outTemp.has_data</td>
+      <td>$emit_boolean($day.outTemp.has_data)</td>
     </tr>
       <tr>
       <td>Test for a bad observation type on a \$day tag: \$day.foobar.min</td>