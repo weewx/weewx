a6f0497ea9e2eecea25f169eb79dd5991f047627
Tom Keffer <tkeffer@gmail.com>
2019-09-05 14:19:26 -0700

    Test _get_temperature_12h_F()

--- bin/weeutil/tests/test_xtypes.py
+++ bin/weeutil/tests/test_xtypes.py
@@ -1,3 +1,12 @@
+#
+#    Copyright (c) 2019 Tom Keffer <tkeffer@gmail.com>
+#
+#    See the file LICENSE.txt for your full rights.
+#
+"""Test ExtendedTypes"""
+
+import unittest
+
 try:
     # Python 3 --- mock is included in unittest
     from unittest import mock
@@ -5,7 +14,6 @@ except ImportError:
     # Python 2 --- must have mock installed
     import mock
 
-import configobj
 import weewx.manager
 import weeutil.xtypes
 
@@ -22,26 +30,39 @@ def new_type(obs_type, record):
         raise ValueError(obs_type)
 
 
-record = {
-    'dateTime': 1567515300, 'usUnits': 1, 'interval': 5, 'barometer': 29.99, 'pressure': 29.259303850622302,
-    'altimeter': 30.001561119603156, 'inTemp': 73.0, 'outTemp': 55.7, 'inHumidity': 54.0, 'outHumidity': 90.0,
-    'windSpeed': 0.0, 'windDir': None, 'windGust': 2.0, 'windGustDir': 270.0, 'rainRate': 0.0, 'rain': 0.0,
-    'dewpoint': 52.81113360826872, 'windchill': 55.7, 'heatindex': 55.7, 'ET': 0.0, 'radiation': 0.0, 'UV': 0.0
-}
+# These are the correct values
+dewpoint = 52.81113360826872
+pressure = 29.259303850622302
+barometer = 29.99
+altimeter = 30.001561119603156
 
-print("Dewpoint before is %f", record['dewpoint'])
-print("Pressure before is %f", record['pressure'])
-del record['dewpoint']
-del record['pressure']
 
-config_dict = configobj.ConfigObj('/home/weewx/weewx.conf', encoding='utf8')
-db_binder = weewx.manager.DBBinder(config_dict)
-db_manager = db_binder.get_manager('wx_binding')
-ps = weeutil.xtypes.PressureCooker(700, db_manager)
+class TestExtendedTypes(unittest.TestCase):
+    """Test the Ambient RESTful protocol"""
 
-with mock.patch.object(db_manager, 'getRecord', return_value={'usUnits': 1, 'outTemp':80.3}) as mock_mgr:
-    t = ps._get_temperature_12h_F(record['dateTime'])
-    print(t)
+    def setUp(self):
+        self.record = {
+            'dateTime': 1567515300, 'usUnits': 1, 'interval': 5, 'inTemp': 73.0, 'outTemp': 55.7, 'inHumidity': 54.0,
+            'outHumidity': 90.0, 'windSpeed': 0.0, 'windDir': None, 'windGust': 2.0, 'windGustDir': 270.0,
+            'rainRate': 0.0, 'rain': 0.0, 'windchill': 55.7, 'heatindex': 55.7, 'ET': 0.0, 'radiation': 0.0, 'UV': 0.0
+        }
+
+    def test_get_temperature_12h_F(self):
+        db_manager = mock.Mock()
+        ps = weeutil.xtypes.PressureCooker(700, db_manager)
+
+        # Mock a database in US units
+        with mock.patch.object(db_manager, 'getRecord', return_value={'usUnits': weewx.US, 'outTemp': 80.3}) as mock_mgr:
+            t = ps._get_temperature_12h_F(self.record['dateTime'])
+            mock_mgr.assert_called_once_with(self.record['dateTime'] - 12*3600, max_delta=1800)
+            self.assertEqual(t, 80.3)
+
+        # Mock a database in METRICWX units
+        with mock.patch.object(db_manager, 'getRecord', return_value={'usUnits': weewx.METRICWX, 'outTemp': 30.0}) as mock_mgr:
+            t = ps._get_temperature_12h_F(self.record['dateTime'])
+            mock_mgr.assert_called_once_with(self.record['dateTime'] - 12*3600, max_delta=1800)
+            self.assertEqual(t, 86)
+
 #
 # my_types = weeutil.xtypes.ExtendedTypes(record, {'dewpoint': new_type,
 #                                                  'pressure': ps.calc
@@ -49,3 +70,5 @@ with mock.patch.object(db_manager, 'getRecord', return_value={'usUnits': 1, 'out
 #
 # print('dewpoint after =', my_types['dewpoint'])
 # print('pressure after =', my_types['pressure'])
+if __name__ == '__main__':
+    unittest.main()
