c5ac4b017c3e70b694b845220f2f4fa06ff13d41
Tom Keffer <tkeffer@gmail.com>
2019-10-08 15:33:04 -0700

    Minor simplifications

--- bin/weewx/tests/test_accum.py
+++ bin/weewx/tests/test_accum.py
@@ -11,23 +11,25 @@ import weewx.accum
 from gen_fake_data import genFakeRecords
 
 # 30 minutes worth of data:
-start_ts = int(time.mktime((2009,1,1,0, 0,0,0,0,-1)))
-stop_ts  = int(time.mktime((2009,1,1,0,30,0,0,0,-1)))
+start_ts = int(time.mktime((2009, 1, 1, 0, 0, 0, 0, 0, -1)))
+stop_ts = int(time.mktime((2009, 1, 1, 0, 30, 0, 0, 0, -1)))
+
 
 class AccumTest(unittest.TestCase):
-    
+
     def setUp(self):
 
-        self.dataset = [record for record in genFakeRecords(start_ts=start_ts, stop_ts=stop_ts, interval=5)]
+        # The data set is a list of faked records at 5 second intervals
+        self.dataset = list(genFakeRecords(start_ts=start_ts, stop_ts=stop_ts, interval=5))
 
     def test_scalarStats(self):
-        
+
         ss = weewx.accum.ScalarStats()
-        
+
         # Make sure the default values work:
         self.assertEqual(ss.min, None)
         self.assertEqual(ss.last, None)
-        
+
         tmin = tmintime = None
         tsum = tcount = 0
         for record in self.dataset:
@@ -37,34 +39,34 @@ class AccumTest(unittest.TestCase):
                 if tmin is None or record['outTemp'] < tmin:
                     tmin = record['outTemp']
                     tmintime = record['dateTime']
-                    
+
             ss.addHiLo(record['outTemp'], record['dateTime'])
             ss.addSum(record['outTemp'])
-        
+
         self.assertEqual(ss.min, tmin)
         self.assertEqual(ss.mintime, tmintime)
-        
+
         # Assumes the last data point is not None:
-        self.assertEqual(ss.last,     self.dataset[-1]['outTemp'])
+        self.assertEqual(ss.last, self.dataset[-1]['outTemp'])
         self.assertEqual(ss.lasttime, self.dataset[-1]['dateTime'])
-        
+
         self.assertEqual(ss.sum, tsum)
         self.assertEqual(ss.count, tcount)
-        self.assertEqual(ss.avg, tsum/tcount)
-        
+        self.assertEqual(ss.avg, tsum / tcount)
+
         # Merge ss into its self. Should leave highs and lows unchanged, but double counts:
         ss.mergeHiLo(ss)
         ss.mergeSum(ss)
-        
+
         self.assertEqual(ss.min, tmin)
         self.assertEqual(ss.mintime, tmintime)
-        
-        self.assertEqual(ss.last,     self.dataset[-1]['outTemp'])
+
+        self.assertEqual(ss.last, self.dataset[-1]['outTemp'])
         self.assertEqual(ss.lasttime, self.dataset[-1]['dateTime'])
-        
-        self.assertEqual(ss.sum, 2*tsum)
-        self.assertEqual(ss.count, 2*tcount)
-        
+
+        self.assertEqual(ss.sum, 2 * tsum)
+        self.assertEqual(ss.count, 2 * tcount)
+
+
 if __name__ == '__main__':
     unittest.main()
-            
