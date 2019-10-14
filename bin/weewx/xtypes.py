5d4ff25f76a385c2d5adc427dd185d4622d1ceab
Tom Keffer <tkeffer@gmail.com>
2019-10-14 04:58:53 -0700

    More details on type extensions.

--- bin/weewx/xtypes.py
+++ bin/weewx/xtypes.py
@@ -4,17 +4,28 @@
 #    See the file LICENSE.txt for your full rights.
 #
 """User-defined, extended types"""
-from __future__ import print_function
 
 import weewx
 
+#
+# List of type extensions. Each should be a function, with the calling signature
+#   fn(key, record, db_manager)
+# where
+#   key is the observation type to be calculated(e.g., 'dewpoint');
+#   record is a dictionary holding the observation data;
+#   db_manager is an open instance of weewx.manager.Manager.
+#
 scalar_types = []
 
 
 def get_scalar(key, record, db_manager=None):
+    """Search the list, looking for the key 'key'."""
     for xtype in scalar_types:
         try:
+            # Try this function. It will raise an exception if it does not know about the type.
             return xtype(key, record, db_manager)
         except weewx.UnknownType:
+            # This function does not know about the type. Move on to the next one.
             pass
+    # None of the functions worked.
     raise weewx.UnknownType(key)
