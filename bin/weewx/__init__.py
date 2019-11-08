c93d8603664b74ef81a667a4434319f375798b5d
Tom Keffer <tkeffer@gmail.com>
2019-11-08 09:14:39 -0800

    version 4.0.0xt1, incorporating extended types.

--- bin/weewx/__init__.py
+++ bin/weewx/__init__.py
@@ -7,7 +7,7 @@
 from __future__ import absolute_import
 import time
 
-__version__="4.0.0a9"
+__version__="4.0.0xt1"
 
 # Holds the program launch time in unix epoch seconds:
 # Useful for calculating 'uptime.'
