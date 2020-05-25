a55c9cd22b210b10e412ec8d6d7066947af1d9a6
Tom Keffer <tkeffer@gmail.com>
2020-05-25 12:10:00 -0700

    Set Version to 4.1.0

--- bin/weewx/__init__.py
+++ bin/weewx/__init__.py
@@ -7,7 +7,7 @@
 from __future__ import absolute_import
 import time
 
-__version__="4.0.0"
+__version__="4.1.0"
 
 # Holds the program launch time in unix epoch seconds:
 # Useful for calculating 'uptime.'
