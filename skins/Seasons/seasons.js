bb6415e8d049c25e1805b822bf6a574e6b6ec8f3
Tom Keffer <tkeffer@gmail.com>
2020-10-24 12:01:01 -0700

    Modernize Javascript

--- skins/Seasons/seasons.js
+++ skins/Seasons/seasons.js
@@ -3,22 +3,22 @@
  * Distributed under terms of GPLv3.  See LICENSE.txt for your rights.
  */
 
-var cookie_prefix = "weewx.seasons.";
-var year_type = get_state('year_type', 'year');
+const cookie_prefix = "weewx.seasons.";
+let year_type = get_state('year_type', 'year');
 
 function setup(widgets) {
     // set the state of the history widget
-    var id = get_state('history', 'day');
+    const id = get_state('history', 'day');
     choose_history(id);
     // if we got a list of widget names, then use it.  otherwise, query the doc
     // for every object with an id of *_widget, and use that as the name list.
     if (!widgets) {
         widgets = [];
-        var items = document.getElementsByClassName('widget');
+        const items = document.getElementsByClassName('widget');
         if (items) {
-            for (var i = 0; i < items.length; i++) {
+            for (let i = 0; i < items.length; i++) {
                 if (items[i].id) {
-                    var widget_name = items[i].id.replace('_widget', '');
+                    const widget_name = items[i].id.replace('_widget', '');
                     if (widget_name) {
                         widgets.push(widget_name);
                     }
@@ -27,8 +27,8 @@ function setup(widgets) {
         }
     }
     // now set the toggle state for each widget based on what the cookies say
-    for (i = 0; i < widgets.length; i++) {
-        var state = get_state(widgets[i] + '.state', 'expanded');
+    for (let i = 0; i < widgets.length; i++) {
+        const state = get_state(widgets[i] + '.state', 'expanded');
         toggle_widget(widgets[i], state);
     }
 }
@@ -40,26 +40,26 @@ function choose_history(id) {
 }
 
 function choose_rainyear(id) {
-    if (id == 'year') {
+    if (id === 'year') {
         choose_col('hilo', year_type, ['year', 'rainyear']);
     }
 }
 
 function toggle_rainyear() {
-    if (year_type == 'year') {
+    if (year_type === 'year') {
         year_type = 'rainyear';
     } else {
         year_type = 'year';
     }
     set_state('year_type', year_type);
-    var id = get_active_div('history', ['day', 'week', 'month', 'year'], 'day');
+    const id = get_active_div('history', ['day', 'week', 'month', 'year'], 'day');
     choose_rainyear(id);
 }
 
 function toggle_widget(id, state) {
-    var id_elements = document.getElementById(id + '_widget');
+    const id_elements = document.getElementById(id + '_widget');
     if (id_elements) {
-        for (var i = 0; i < id_elements.childNodes.length; i++) {
+        for (let i = 0; i < id_elements.childNodes.length; i++) {
             if (id_elements.childNodes[i].className === 'widget_contents') {
                 if (state === undefined) {
                     // make it the opposite of the current state
@@ -73,11 +73,11 @@ function toggle_widget(id, state) {
 }
 
 function choose_col(group, selected_id, all_ids) {
-    for (var i = 0; i < all_ids.length; i++) {
-        var elements = document.getElementsByClassName(group + '_' + all_ids[i]);
+    for (let i = 0; i < all_ids.length; i++) {
+        let elements = document.getElementsByClassName(group + '_' + all_ids[i]);
         if (elements) {
-            var display = selected_id === all_ids[i] ? '' : 'none';
-            for (var j = 0; j < elements.length; j++) {
+            const display = selected_id === all_ids[i] ? '' : 'none';
+            for (let j = 0; j < elements.length; j++) {
                 elements[j].style.display = display;
             }
         }
@@ -85,12 +85,12 @@ function choose_col(group, selected_id, all_ids) {
 }
 
 function choose_div(group, selected_id, all_ids) {
-    for (var i = 0; i < all_ids.length; i++) {
-        var button = document.getElementById('button_' + group + '_' + all_ids[i]);
+    for (let i = 0; i < all_ids.length; i++) {
+        const button = document.getElementById('button_' + group + '_' + all_ids[i]);
         if (button) {
             button.className = (all_ids[i] === selected_id) ? 'button_selected' : 'button';
         }
-        var element = document.getElementById(group + '_' + all_ids[i]);
+        const element = document.getElementById(group + '_' + all_ids[i]);
         if (element) {
             element.style.display = (all_ids[i] === selected_id) ? 'block' : 'none';
         }
@@ -100,10 +100,10 @@ function choose_div(group, selected_id, all_ids) {
 
 /* if cookies are disabled, then we must look at page to get state */
 function get_active_div(group, all_ids, default_value) {
-    var id = default_value;
-    for (var i = 0; i < all_ids.length; i++) {
-        var button = document.getElementById('button_' + group + '_' + all_ids[i]);
-        if (button && button.className == 'button_selected') {
+    let id = default_value;
+    for (let i = 0; i < all_ids.length; i++) {
+        const button = document.getElementById('button_' + group + '_' + all_ids[i]);
+        if (button && button.className === 'button_selected') {
             id = all_ids[i];
         }
     }
@@ -111,15 +111,15 @@ function get_active_div(group, all_ids, default_value) {
 }
 
 function set_state(name, value, dur) {
-    var full_name = cookie_prefix + name;
+    const full_name = cookie_prefix + name;
 /*    set_cookie(full_name, value, dur); */
     window.localStorage.setItem(full_name, value);
 }
 
 function get_state(name, default_value) {
-    var full_name = cookie_prefix + name;
+    const full_name = cookie_prefix + name;
 /*    return get_cookie(name, default_value); */
-    var value = window.localStorage.getItem(full_name);
+    let value = window.localStorage.getItem(full_name);
     if (value === undefined || value == null) {
         value = default_value;
     }
@@ -128,29 +128,29 @@ function get_state(name, default_value) {
 
 function set_cookie(name, value, dur) {
     if (!dur) dur = 30;
-    var today = new Date();
-    var expire = new Date();
+    const today = new Date();
+    let expire = new Date();
     expire.setTime(today.getTime() + 24 * 3600000 * dur);
     document.cookie = name + "=" + encodeURI(value) + ";expires=" + expire.toUTCString();
 }
 
 function get_cookie(name, default_value) {
     if (name === "") return default_value;
-    var cookie = " " + document.cookie;
-    var i = cookie.indexOf(" " + name + "=");
+    const cookie = " " + document.cookie;
+    let i = cookie.indexOf(" " + name + "=");
     if (i < 0) i = cookie.indexOf(";" + name + "=");
     if (i < 0) return default_value;
-    var j = cookie.indexOf(";", i + 1);
+    let j = cookie.indexOf(";", i + 1);
     if (j < 0) j = cookie.length;
     return unescape(cookie.substring(i + name.length + 2, j));
 }
 
 function get_parameter(name) {
-    var query = window.location.search.substring(1);
+    const query = window.location.search.substring(1);
     if (query) {
-        var vars = query.split("&");
-        for (var i = 0; i < vars.length; i++) {
-            var pair = vars[i].split("=");
+        const vars = query.split("&");
+        for (let i = 0; i < vars.length; i++) {
+            const pair = vars[i].split("=");
             if (pair[0] === name) {
                 return pair[1];
             }
@@ -160,13 +160,13 @@ function get_parameter(name) {
 }
 
 function load_file(div_id, var_name) {
-    var content = '';
-    var file = get_parameter(var_name);
+    let content;
+    const file = get_parameter(var_name);
     if (file) {
         content = "Loading " + file;
-        var xhr = new XMLHttpRequest();
+        let xhr = new XMLHttpRequest();
         xhr.onload = function () {
-            var e = document.getElementById(div_id);
+            let e = document.getElementById(div_id);
             if (e) {
                 e.textContent = this.responseText;
             }
@@ -176,7 +176,7 @@ function load_file(div_id, var_name) {
     } else {
         content = 'nothing specified';
     }
-    var e = document.getElementById(div_id);
+    let e = document.getElementById(div_id);
     if (e) {
         e.innerHTML = content;
     }
