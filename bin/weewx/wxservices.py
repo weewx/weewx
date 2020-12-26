63c145d416fac5e853e923517dc528cd7fcd9732
Graham Eddy <graham.eddy@gmail.com>
2020-12-26 19:49:13 +1100

    expand directives in [[Calculations]] to allow bindings to be optionally specified (none specified = all bindings) e.g. mydata = software,archive

--- bin/weewx/wxservices.py
+++ bin/weewx/wxservices.py
@@ -23,8 +23,18 @@ class StdWXCalculate(weewx.engine.StdService):
         """Initialize an instance of StdWXXTypes"""
         super(StdWXCalculate, self).__init__(engine, config_dict)
 
-        # Get the dictionary containing the calculations to be done. Default to no calculations
-        self.calc_dict = config_dict.get('StdWXCalculate', {}).get('Calculations', {})
+        # Get the dictionary containing the calculations to be done, including
+        # the optional bindings. Default to no calculations
+        # TODO update 4.3.0 Customization Guide to show this new option
+        self.calc_dict = dict()
+        for obs_type, directive in config_dict.get('StdWXCalculate', {}).get('Calculations', {}).items():
+            # flatten directive to comma-separated string if it is a list
+            if isinstance(directive, list):
+                directive = ','.join(directive)
+            # remember this calculation
+            self.calc_dict[obs_type] = directive
+        if weewx.debug > 2:
+            log.debug(f"{self.__class__.__name__} calc_dict={self.calc_dict}")
 
         # Backwards compatibility for configuration files v4.1 or earlier:
         self.calc_dict.setdefault('windDir', 'software')
@@ -42,25 +52,33 @@ class StdWXCalculate(weewx.engine.StdService):
         self.bind(weewx.NEW_ARCHIVE_RECORD, self.new_archive_record)
 
     def new_loop_packet(self, event):
-        self.do_calculations(event.packet)
+        self.do_calculations(event.packet, 'loop')
 
     def new_archive_record(self, event):
-        self.do_calculations(event.record)
+        self.do_calculations(event.record, 'archive')
 
-    def do_calculations(self, data_dict):
+    def do_calculations(self, data_dict, this_binding):
         """Augment the data dictionary with derived types as necessary.
 
         data_dict: The incoming LOOP packet or archive record.
+        this_binding: 'loop' or 'archive'
         """
 
         # Go through the list of potential calculations and see which ones need to be done
         for obs in self.calc_dict:
-            directive = self.calc_dict[obs]
+            directive = self.calc_dict[obs]     # preference,binding,binding,..
+
+            # is this calculation to be done for this packet's binding?
+            # it is, if binding in directive, or no bindings (i.e. all included)
+            if ',' in directive and this_binding not in directive:
+                # no - skip
+                continue
+
             # Keys in calc_dict are in unicode. Keys in packets and records are in native strings.
             # Just to keep things consistent, convert.
             obs_type = str(obs)
-            if directive == 'software' or directive == 'prefer_hardware' \
-                    and (obs_type not in data_dict or data_dict[obs_type] is None):
+            if 'software' in directive or \
+               'prefer_hardware' in directive and (obs_type not in data_dict or data_dict[obs_type] is None):
                 try:
                     # We need to do a calculation for type 'obs_type'. This may raise an exception.
                     new_value = weewx.xtypes.get_scalar(obs_type, data_dict, self.db_manager)
