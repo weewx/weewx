c1d340b818a2aa5eebbd5bed7532a8c119c4b4f0
Tom Keffer <tkeffer@gmail.com>
2020-12-31 15:43:09 -0800

    Refactored into smaller test units.

--- bin/weecfg/tests/test_config.py
+++ bin/weecfg/tests/test_config.py
@@ -139,8 +139,8 @@ class ConfigTest(LineTest):
             self.assertRaises(IOError, weecfg.find_file,
                               None, file_name=filename + "foo", locations=['/usr/bin', dir_path])
 
-    def test_utilities(self):
-        global X_STR, Y_STR
+    def test_reorder_before(self):
+        global X_STR
 
         xio = StringIO(X_STR)
         x_dict = configobj.ConfigObj(xio, encoding='utf-8')
@@ -157,7 +157,10 @@ class ConfigTest(LineTest):
         d = 4
 """)
 
-        xio.seek(0)
+    def test_reorder_after(self):
+        global X_STR
+
+        xio = StringIO(X_STR)
         x_dict = configobj.ConfigObj(xio, encoding='utf-8')
         weecfg.reorder_sections(x_dict, 'section_c', 'section_b', after=True)
         x_dict_str = convert_to_str(x_dict)
@@ -171,6 +174,9 @@ class ConfigTest(LineTest):
 [section_d]
         d = 4
 """)
+
+    def test_conditional_merge(self):
+        global X_STR, Y_STR
 
         xio = StringIO(X_STR)
         yio = StringIO(Y_STR)
@@ -191,6 +197,9 @@ class ConfigTest(LineTest):
         c = 15
 """)
 
+    def test_remove_and_prune(self):
+        global X_STR, Y_STR
+
         xio = StringIO(X_STR)
         yio = StringIO(Y_STR)
         x_dict = configobj.ConfigObj(xio, encoding='utf-8')
@@ -204,6 +213,7 @@ class ConfigTest(LineTest):
         d = 4
 """)
 
+    def test_reorder_scalars(self):
         test_list = ['a', 'b', 'd', 'c']
         weecfg.reorder_scalars(test_list, 'c', 'd')
         self.assertEqual(test_list, ['a', 'b', 'c', 'd'])
