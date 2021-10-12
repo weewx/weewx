cd99ddb70d1f16163afca0bc2cae6b5420ffb7c3
Tom Keffer <tkeffer@gmail.com>
2021-10-11 17:17:37 -0700

    V4.6.0b4

--- bin/weewx/__init__.py
+++ bin/weewx/__init__.py
@@ -7,7 +7,7 @@
 from __future__ import absolute_import
 import time
 
-__version__="4.6.0b3"
+__version__="4.6.0b4"
 
 # Holds the program launch time in unix epoch seconds:
 # Useful for calculating 'uptime.'
