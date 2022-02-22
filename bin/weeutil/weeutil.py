458283d917e1fa6b3ae710a5c13ef4a972dd518f
gjr80 <gjroderick@gmail.com>
2022-02-22 12:14:38 +1000

    add keys() and values() methods to the python 2 ListOfDicts() class

--- bin/weeutil/weeutil.py
+++ bin/weeutil/weeutil.py
@@ -1550,6 +1550,14 @@ except ImportError:
 
         __copy__ = copy
 
+        def keys(self):
+            """Return a list of all keys in the maps."""
+            return [i for s in self.maps for i in s.keys()]
+
+        def values(self):
+            """Return a list of all values in the maps."""
+            return [i for s in self.maps for i in s.values()]
+
 
 class KeyDict(dict):
     """A dictionary that returns the key for an unsuccessful lookup."""
