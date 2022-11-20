8fd28dbe9181ae5b5723260438c13be0d952a6b2
Tom Keffer <tkeffer@gmail.com>
2022-11-20 11:19:44 -0800

    Move class Stopper to its own module
    Originally, this class was included in test_engine.py. When
    test_engine.py was run, it would set up the logger. Then it would run
    the engine, which would dynamically load "test_engine.Stopper", which
    caused test_engine to be loaded *again*, resulting in the logger getting
    set up a 2nd time. This resulted in "ResourceWarning" errors because the
    syslog socket connection from the first setup was not closed.

--- bin/weewx/tests/test_engine.py
+++ bin/weewx/tests/test_engine.py
@@ -1,5 +1,5 @@
 #
-#    Copyright (c) 2009-2019 Tom Keffer <tkeffer@gmail.com>
+#    Copyright (c) 2009-2022 Tom Keffer <tkeffer@gmail.com>
 #
 #    See the file LICENSE.txt for your full rights.
 #
@@ -19,57 +19,60 @@ import configobj
 from six.moves import zip
 
 import weedb
+import weeutil.config
 import weeutil.weeutil
 import weewx.drivers.simulator
 import weewx.engine
 import weewx.manager
+from weeutil.weeutil import to_int
 
 weewx.debug = 1
 
 log = logging.getLogger(__name__)
-# Set up logging using the defaults.
-weeutil.logger.setup('test_engine', {})
 
 os.environ['TZ'] = 'America/Los_Angeles'
 time.tzset()
 
-RUN_LENGTH = 48.0  # How long to run the simulator in hours.
 # The types to actually test:
 TEST_TYPES = ['outTemp', 'inTemp', 'barometer', 'windSpeed']
 
 # Find the configuration file. It's assumed to be in the same directory as me:
 config_path = os.path.join(os.path.dirname(__file__), "simgen.conf")
 
+try:
+    config_dict = configobj.ConfigObj(config_path, file_error=True, encoding='utf-8')
+except IOError:
+    print("Unable to open configuration file %s" % config_path, file=sys.stderr)
+    # Reraise the exception (this will eventually cause the program to exit)
+    raise
+except configobj.ConfigObjError:
+    print("Error while parsing configuration file %s" % config_path, file=sys.stderr)
+    raise
+
+weeutil.logger.setup('test_engine', config_dict)
+
 
 class TestEngine(unittest.TestCase):
     """Test the engine and accumulators using the simulator."""
 
     def setUp(self):
-        global config_path
-
-        try:
-            self.config_dict = configobj.ConfigObj(config_path, file_error=True, encoding='utf-8')
-        except IOError:
-            print("Unable to open configuration file %s" % config_path, file=sys.stderr)
-            # Reraise the exception (this will eventually cause the program to exit)
-            raise
-        except configobj.ConfigObjError:
-            print("Error while parsing configuration file %s" % config_path, file=sys.stderr)
-            raise
+        global config_dict
+        self.config_dict = weeutil.config.deep_copy(config_dict)
 
         first_ts, last_ts = _get_first_last(self.config_dict)
 
         try:
-            with weewx.manager.open_manager_with_config(self.config_dict, 'wx_binding') as dbmanager:
+            with weewx.manager.open_manager_with_config(self.config_dict,
+                                                        'wx_binding') as dbmanager:
                 if dbmanager.firstGoodStamp() == first_ts and dbmanager.lastGoodStamp() == last_ts:
                     print("\nSimulator need not be run")
                     return
         except weedb.OperationalError:
             pass
 
-        # This will generate the simulator data:
         engine = weewx.engine.StdEngine(self.config_dict)
         try:
+            # This will generate the simulator data:
             engine.run()
         except weewx.StopNow:
             pass
@@ -87,34 +90,13 @@ class TestEngine(unittest.TestCase):
                     self.assertAlmostEqual(obs_avg[obs_type], record[obs_type], 2)
 
 
-class Stopper(weewx.engine.StdService):
-    """Special service which stops the engine when it gets to a certain time."""
-
-    def __init__(self, engine, config_dict):
-        global RUN_LENGTH
-        super(Stopper, self).__init__(engine, config_dict)
-
-        self.first_ts, self.last_ts = _get_first_last(config_dict)
-        self.count = 0
-
-        self.bind(weewx.NEW_ARCHIVE_RECORD, self.new_archive_record)
-
-    def new_archive_record(self, event):
-        self.count += 1
-        print("~", end='')
-        if self.count % 80 == 0:
-            print("")
-        sys.stdout.flush()
-        if event.record['dateTime'] >= self.last_ts:
-            raise weewx.StopNow("Time to stop!")
-
-
 def _get_first_last(config_dict):
     """Get the first and last archive record timestamps."""
+    run_length = to_int(config_dict['Stopper']['run_length'])
     start_tt = time.strptime(config_dict['Simulator']['start'], "%Y-%m-%dT%H:%M")
     start_ts = time.mktime(start_tt)
     first_ts = start_ts + config_dict['StdArchive'].as_int('archive_interval')
-    last_ts = start_ts + RUN_LENGTH * 3600.0
+    last_ts = start_ts + run_length * 3600.0
     return first_ts, last_ts
 
 
@@ -125,10 +107,11 @@ def calc_stats(config_dict, start_ts, stop_ts):
     sim_start_tt = time.strptime(config_dict['Simulator']['start'], "%Y-%m-%dT%H:%M")
     sim_start_ts = time.mktime(sim_start_tt)
 
-    simulator = weewx.drivers.simulator.Simulator(loop_interval=config_dict['Simulator'].as_int('loop_interval'),
-                                                  mode='generator',
-                                                  start_time=sim_start_ts,
-                                                  resume_time=start_ts)
+    simulator = weewx.drivers.simulator.Simulator(
+        loop_interval=config_dict['Simulator'].as_int('loop_interval'),
+        mode='generator',
+        start_time=sim_start_ts,
+        resume_time=start_ts)
 
     obs_sum = dict(zip(TEST_TYPES, len(TEST_TYPES) * (0,)))
     obs_min = dict(zip(TEST_TYPES, len(TEST_TYPES) * (None,)))
