2e18f9d3743e3fb732a2027a4cdf76ced58cce0a
gary <gary@therodericks.id.au>
2022-11-23 21:06:14 +1000

    - revised config files - rename schema file - tidy up and minor refactoring of test_weeimport.py

--- bin/weeimport/tests/test_weeimport.py
+++ bin/weeimport/tests/test_weeimport.py
@@ -28,78 +28,36 @@ import weeutil.logger
 
 weeutil.logger.setup('test_weeimport',{})
 
-archive_sqlite = {'database_name': '/var/tmp/weewx_test/weedb.sdb', 'driver':'weedb.sqlite'}
-archive_mysql  = {'database_name': 'test_weedb', 'user':'weewx1', 'password':'weewx1', 'driver':'weedb.mysql'}
-
-archive_schema = [('dateTime',             'INTEGER NOT NULL UNIQUE PRIMARY KEY'),
-                  ('usUnits',              'INTEGER NOT NULL'),
-                  ('interval',             'INTEGER NOT NULL'),
-                  ('barometer',            'REAL'),
-                  ('inTemp',               'REAL'),
-                  ('outTemp',              'REAL'),
-                  ('windSpeed',            'REAL')]
-
-std_unit_system = 1
-interval = 3600     # One hour
-nrecs = 48          # Two days
-start_ts = int(time.mktime((2012, 7, 1, 00, 00, 0, 0, 0, -1))) # 1 July 2012
-stop_ts = start_ts + interval * (nrecs-1)
-timevec = [start_ts+i*interval for i in range(nrecs)]
-
-# Find the configuration file. It's assumed to be in the same directory as me, so first figure
-# out where that is.
+import_test_working_dir = '/var/tmp/import_test'
+# Find the config files used by the tests. they are assumed to be in the same
+# directory as me, so first figure out where that is.
 my_dir = os.path.normpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-# The full path to the configuration file:
+# the full path to the configuration file
 test_import_config_path = os.path.join(my_dir, "test_import.conf")
+# the full path to the csv import configuration file
 test_import_csv_import_config_path = os.path.join(my_dir, "test_csv_import.conf")
-
-
-def timefunc(i):
-    return start_ts + i * interval
-
-
-def barfunc(i):
-    return 30.0 + 0.01 * i
-
-
-def temperfunc(i):
-    return 68.0 + 0.1 * i
-
-
-def expected_record(irec):
-    _record = {'dateTime': timefunc(irec), 'interval': int(interval / 60), 'usUnits': 1,
-               'outTemp': temperfunc(irec), 'barometer': barfunc(irec),
-               'inTemp': 70.0 + 0.1 * irec}
-    return _record
-
-
-def gen_included_recs(timevec, start_ts, stop_ts, agg_interval):
-    """Generator function that marches down a set of aggregation intervals. Each yield returns
-     the set of records included in that interval."""
-    for span in weeutil.weeutil.intervalgen(start_ts, stop_ts, agg_interval):
-        included = set()
-        for (irec, ts) in enumerate(timevec):
-            if span[0] < ts <= span[1]:
-                included.add(irec)
-        yield included
-
-
-def genRecords():
-    for irec in range(nrecs):
-        _record = expected_record(irec)
-        yield _record
 
 
 class TestCsvImport(unittest.TestCase):
     """Test CSV import."""
 
-    # cut down import config to test application of defaults
+    test_data_file = 'test_data.csv'
+    test_data = """Time,Barometer,Temp,Humidity,Windspeed,Dir,Gust,Dayrain,Radiation,Uv,Comment
+28/11/2017 08:00:00,1016.9,24.6,84,1.8,113,8,0,359,3.8,"start of observations"
+28/11/2017 08:05:00,1016.9,25.1,82,4.8,135,11.3,0,775,4.7,
+28/11/2017 08:10:00,1016.9,25.4,80,4.4,127,11.3,0,787,5.1,"note temperature"
+28/11/2017 08:15:00,1017,25.7,79,3.5,74,11.3,0,800,5.4,
+28/11/2017 08:20:00,1016.9,25.9,79,1.6,95,9.7,0,774,5.5,
+28/11/2017 08:25:00,1017,25.5,78,2.9,48,9.7,0,303,3.4,"forecast received"
+28/11/2017 08:30:00,1017.1,25.1,80,3.1,54,9.7,0,190,3.6,"""
+
+    # minimal import config to test application of defaults
     default_import_config = """
-    file = /var/tmp/data.csv
+    file = %s/%s
     [FieldMap]
         dateTime    = timestamp, unix_epoch
         interval    =
-        barometer   = barometer, inHg"""
+        barometer   = barometer, inHg""" % (import_test_working_dir, TestCsvImport.test_data_file)
 
     class Options(object):
         """Class to represent command line options fed to wee_import.
@@ -138,21 +96,18 @@ class TestCsvImport(unittest.TestCase):
         # obtain an Options object to hold any 'wee_import' command line
         # options
         self.options = TestCsvImport.Options()
-        # now we can get a CsvSource object
-        self.csv_source_obj = weeimport.csvimport.CSVSource(self.config_dict,
-                                                            self.config_path,
-                                                            self.import_config_dict,
-                                                            self.import_config_path,
-                                                            self.options)
-        # get a CsvSource object based upon defaults only, first get a minimal
-        # import config
-        minimal_import_config_dict = configobj.ConfigObj(StringIO(TestCsvImport.default_import_config))
-        # now get a CsvSource object based on this minimal import config
-        self.csv_source_obj_defaults = weeimport.csvimport.CSVSource(self.config_dict,
-                                                                     self.config_path,
-                                                                     minimal_import_config_dict,
-                                                                     self.import_config_path,
-                                                                     self.options)
+        # generate the file containing the CSV test data to be imported
+        with open(os.path.join(import_test_working_dir, TestCsvImport.test_data_file), 'w') as f:
+            f.write(TestCsvImport.test_data)
+
+    def tearDown(self):
+        """Clean up after ourselves."""
+
+        # delete the CSV import test data
+        try:
+            os.remove(os.path.join(import_test_working_dir, TestCsvImport.test_data_file))
+        except (OSError, FileNotFoundError):
+            pass
 
     def test_csv_init(self):
         """Test CsvSource object initialisation.
@@ -161,32 +116,39 @@ class TestCsvImport(unittest.TestCase):
         dicts and command line options.
         """
 
+        # get a CsvSource object
+        csv_source_obj = weeimport.csvimport.CSVSource(self.config_dict,
+                                                       self.config_path,
+                                                       self.import_config_dict,
+                                                       self.import_config_path,
+                                                       self.options)
+
         # check the import_config_path property is correctly set
-        self.assertEqual(self.csv_source_obj.import_config_path, self.import_config_path)
+        self.assertEqual(csv_source_obj.import_config_path, self.import_config_path)
         # check the csv import config dict is correctly set
-        self.assertEqual(self.csv_source_obj.csv_config_dict, self.import_config_dict)
+        self.assertEqual(csv_source_obj.csv_config_dict, self.import_config_dict)
         # check the delimiter property is correctly set
-        self.assertEqual(self.csv_source_obj.delimiter, ',')
+        self.assertEqual(csv_source_obj.delimiter, ',')
         # check the raw_datetime_format property is correctly set
-        self.assertEqual(self.csv_source_obj.raw_datetime_format, '%Y-%m-%d %H:%M:%S')
+        self.assertEqual(csv_source_obj.raw_datetime_format, '%Y-%m-%d %H:%M:%S')
         # check the rain property is correctly set
-        self.assertEqual(self.csv_source_obj.rain, 'cumulative')
+        self.assertEqual(csv_source_obj.rain, 'cumulative')
         # check the wind_dir property is correctly set
-        self.assertEqual(self.csv_source_obj.wind_dir, [-360, 360])
+        self.assertEqual(csv_source_obj.wind_dir, [-360, 360])
         # check the source property is correctly set
-        self.assertEqual(self.csv_source_obj.source, self.import_config_dict.get('file'))
+        self.assertEqual(csv_source_obj.source, self.import_config_dict.get('file'))
         # check the source_encoding property is correctly set
-        self.assertEqual(self.csv_source_obj.source_encoding, 'utf-8-sig')
+        self.assertEqual(csv_source_obj.source_encoding, 'utf-8-sig')
         # check the map property is correctly set
-        self.assertIsNone(self.csv_source_obj.map)
+        self.assertIsNone(csv_source_obj.map)
         # check the start property is correctly set
-        self.assertEqual(self.csv_source_obj.start, 1)
+        self.assertEqual(csv_source_obj.start, 1)
         # check the end property is correctly set
-        self.assertEqual(self.csv_source_obj.end, 1)
+        self.assertEqual(csv_source_obj.end, 1)
         # check the increment property is correctly set
-        self.assertEqual(self.csv_source_obj.increment, 1)
+        self.assertEqual(csv_source_obj.increment, 1)
         # check the verbose property is correctly set
-        self.assertIsNone(self.csv_source_obj.verbose)
+        self.assertIsNone(csv_source_obj.verbose)
 
     def test_csv_defaults(self):
         """Test CsvSource object defaults.
@@ -194,32 +156,75 @@ class TestCsvImport(unittest.TestCase):
         Test that class CsvSource defaults are correctly applied.
         """
 
+        # get a CsvSource object based upon defaults only, first get a minimal
+        # import config
+        minimal_import_config = configobj.ConfigObj(StringIO(TestCsvImport.default_import_config))
+        # now get a CsvSource object based on this minimal import config
+        csv_source_obj_defaults = weeimport.csvimport.CSVSource(self.config_dict,
+                                                                self.config_path,
+                                                                minimal_import_config,
+                                                                self.import_config_path,
+                                                                self.options)
         # check the delimiter property is set to the default
-        self.assertEqual(self.csv_source_obj_defaults.delimiter, ',')
+        self.assertEqual(csv_source_obj_defaults.delimiter, ',')
         # check the raw_datetime_format property is set to the default
-        self.assertEqual(self.csv_source_obj_defaults.raw_datetime_format, '%Y-%m-%d %H:%M:%S')
+        self.assertEqual(csv_source_obj_defaults.raw_datetime_format, '%Y-%m-%d %H:%M:%S')
         # check the rain property is set to the default
-        self.assertEqual(self.csv_source_obj_defaults.rain, 'cumulative')
+        self.assertEqual(csv_source_obj_defaults.rain, 'cumulative')
         # check the wind_dir property is set to the default
-        self.assertEqual(self.csv_source_obj_defaults.wind_dir, [0, 360])
+        self.assertEqual(csv_source_obj_defaults.wind_dir, [0, 360])
         # check the source_encoding property is set to the default
-        self.assertEqual(self.csv_source_obj_defaults.source_encoding, 'utf-8-sig')
+        self.assertEqual(csv_source_obj_defaults.source_encoding, 'utf-8-sig')
         # check the interval property is set to the default
-        self.assertEqual(self.csv_source_obj_defaults.interval, 'derive')
+        self.assertEqual(csv_source_obj_defaults.interval, 'derive')
         # check the ignore_invalid_data property is set to the default
-        self.assertEqual(self.csv_source_obj_defaults.ignore_invalid_data, True)
+        self.assertEqual(csv_source_obj_defaults.ignore_invalid_data, True)
         # check the tranche property is set to the default
-        self.assertEqual(self.csv_source_obj_defaults.tranche, 250)
+        self.assertEqual(csv_source_obj_defaults.tranche, 250)
         # check the apply_qc property is set to the default
-        self.assertEqual(self.csv_source_obj_defaults.apply_qc, True)
+        self.assertEqual(csv_source_obj_defaults.apply_qc, True)
         # check the calc_missing property is set to the default
-        self.assertEqual(self.csv_source_obj_defaults.calc_missing, True)
+        self.assertEqual(csv_source_obj_defaults.calc_missing, True)
         # check the decimal_sep property is set to the default
-        self.assertEqual(self.csv_source_obj_defaults.decimal_sep, '.')
+        self.assertEqual(csv_source_obj_defaults.decimal_sep, '.')
         # check the UV_sensor property is set to the default
-        self.assertEqual(self.csv_source_obj_defaults.UV_sensor, True)
+        self.assertEqual(csv_source_obj_defaults.UV_sensor, True)
         # check the solar_sensor property is set to the default
-        self.assertEqual(self.csv_source_obj_defaults.solar_sensor, True)
+        self.assertEqual(csv_source_obj_defaults.solar_sensor, True)
+
+    def test_csv_other(self):
+        """Test CsvSource object generators and property methods.
+
+        Tests the following CsvSource object methods:
+
+        period_generator(). Generator method yielding the import period number.
+                            For a CSV import there is always only one import
+                            period so the generator yields the integer 1 once.
+        first_period. Boolean property that indicates if the current import
+                      period is the first import period. A CSV import has only
+                      one import period so the property is always True.
+        last_period. Boolean property that indicates if the current import
+                     period is the last import period. A CSV import has only
+                     one import period so the property is always True.
+        """
+
+        # get a CsvSource object
+        csv_source_obj = weeimport.csvimport.CSVSource(self.config_dict,
+                                                       self.config_path,
+                                                       self.import_config_dict,
+                                                       self.import_config_path,
+                                                       self.options)
+        period_count = 0
+        for period in csv_source_obj.period_generator():
+            period_count += 1
+            # test the generator yields the integer 1
+            self.assertEqual(period, 1)
+            # test the first_period property is True
+            self.assertTrue(csv_source_obj.first_period)
+            # test the first_period property is True
+            self.assertTrue(csv_source_obj.last_period)
+        # test the generator yielded one value only
+        self.assertEqual(period_count, 1)
 
 
 def suite():
