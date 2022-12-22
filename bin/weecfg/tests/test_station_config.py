abfc867f78736c14e58f74c7dbced5ff1160ed09
Tom Keffer <tkeffer@gmail.com>
2022-12-22 15:03:54 -0800

    Use the real weewx.conf as the test config file

--- bin/weecfg/tests/test_station_config.py
+++ bin/weecfg/tests/test_station_config.py
@@ -5,7 +5,7 @@
 #
 """Test the configuration utilities."""
 import contextlib
-import io
+import importlib.resources
 import os
 import unittest
 from unittest.mock import patch
@@ -19,67 +19,11 @@ import weeutil.config
 import weeutil.weeutil
 import weewx
 
-CONFIG_DICT_STR = """
-# WEEWX TEST CONFIGURATION FILE
+# Use the version of weewx.onf that comes with WeeWX for the tests.
+with importlib.resources.open_text('wee_resources', 'weewx.conf', encoding='utf-8') as fd:
+    CONFIG_DICT = configobj.ConfigObj(fd, encoding='utf-8', file_error=True)
 
-# Set to 1 for extra debug info, otherwise comment it out or set to zero
-debug = 1
-
-# Root directory of the weewx data file hierarchy for this station
-WEEWX_ROOT = /home/weewx
-
-# Whether to log successful operations. May get overridden below.
-log_success = True
-
-# Whether to log unsuccessful operations. May get overridden below.
-log_failure = True
-
-# Do not modify this. It is used when installing and updating weewx.
-version = 4.10.0a1
-
-##############################################################################
-
-#   This section is for information about the station.
-
-[Station]
-
-    # Description of the station location
-    location = "Test station"
-
-    # Latitude in decimal degrees. Negative for southern hemisphere
-    latitude = 5.00
-    # Longitude in decimal degrees. Negative for western hemisphere.
-    longitude = 10.00
-
-    # Altitude of the station, with the unit it is in. This is used only
-    # if the hardware cannot supply a value.
-    altitude = 700, foot    # Choose 'foot' or 'meter' for unit
-
-    # Set to type of station hardware. There must be a corresponding stanza
-    # in this file, which includes a value for the 'driver' option.
-    station_type = unspecified
-
-    # If you have a website, you may specify an URL. This is required if you
-    # intend to register your station.
-    #station_url = http://www.example.com
-
-    # The start of the rain year (1=January; 10=October, etc.). This is
-    # downloaded from the station if the hardware supports it.
-    rain_year_start = 1
-
-[StdRESTful]
-    [[StationRegistry]]
-        register_this_station = false
-        
-[StdReport]
-    [[Defaults]]
-        lang = en
-        unit_system = us
-"""
-
-CONFIG_DICT = configobj.ConfigObj(io.StringIO(CONFIG_DICT_STR))
-
-STATION_URL = 'http://weewx.com'
+STATION_URL = 'https://weewx.com'
 
 
 def suppress_stdout(func):
@@ -89,6 +33,7 @@ def suppress_stdout(func):
                 return func(*args, **kwargs)
 
     return wrapper
+
 
 class CommonConfigTest(unittest.TestCase):
 
@@ -100,10 +45,10 @@ class LocationConfigTest(CommonConfigTest):
 
     def test_default_config_location(self):
         weecfg.station_config.config_location(self.config_dict, no_prompt=True)
-        self.assertEqual(self.config_dict['Station']['location'], "Test station")
+        self.assertEqual(self.config_dict['Station']['location'], "WeeWX station")
         del self.config_dict['Station']['location']
         weecfg.station_config.config_location(self.config_dict, no_prompt=True)
-        self.assertEqual(self.config_dict['Station']['location'], "WeeWX")
+        self.assertEqual(self.config_dict['Station']['location'], "WeeWX station")
 
     def test_arg_config_location(self):
         weecfg.station_config.config_location(self.config_dict, location='foo', no_prompt=True)
@@ -113,10 +58,11 @@ class LocationConfigTest(CommonConfigTest):
     def test_prompt_config_location(self):
         with patch('weecfg.station_config.input', side_effect=['']):
             weecfg.station_config.config_location(self.config_dict)
-            self.assertEqual(self.config_dict['Station']['location'], "Test station")
+            self.assertEqual(self.config_dict['Station']['location'], "WeeWX station")
         with patch('weecfg.station_config.input', side_effect=['bar']):
             weecfg.station_config.config_location(self.config_dict)
             self.assertEqual(self.config_dict['Station']['location'], "bar")
+
 
 class AltitudeConfigTest(CommonConfigTest):
 
@@ -167,8 +113,8 @@ class LatLonConfigTest(CommonConfigTest):
     def test_default_config_latlon(self):
         # Use the default as supplied by CONFIG_DICT
         weecfg.station_config.config_latlon(self.config_dict, no_prompt=True)
-        self.assertEqual(float(self.config_dict['Station']['latitude']), 5.0)
-        self.assertEqual(float(self.config_dict['Station']['longitude']), 10.0)
+        self.assertEqual(float(self.config_dict['Station']['latitude']), 0.0)
+        self.assertEqual(float(self.config_dict['Station']['longitude']), 0.0)
         # Delete the values in the configuration dictionary
         del self.config_dict['Station']['latitude']
         del self.config_dict['Station']['longitude']
@@ -178,13 +124,13 @@ class LatLonConfigTest(CommonConfigTest):
         self.assertEqual(float(self.config_dict['Station']['longitude']), 0.0)
 
     def test_arg_config_latlon(self):
-        weecfg.station_config.config_latlon(self.config_dict, latitude=-20, longitude=-40)
+        weecfg.station_config.config_latlon(self.config_dict, latitude='-20', longitude='-40')
         self.assertEqual(float(self.config_dict['Station']['latitude']), -20.0)
         self.assertEqual(float(self.config_dict['Station']['longitude']), -40.0)
 
     def test_badarg_config_latlon(self):
         with self.assertRaises(ValueError):
-            weecfg.station_config.config_latlon(self.config_dict, latitude="-20f", longitude=-40)
+            weecfg.station_config.config_latlon(self.config_dict, latitude="-20f", longitude='-40')
 
     @suppress_stdout
     def test_prompt_config_latlong(self):
@@ -234,7 +180,7 @@ class RegistryConfigTest(CommonConfigTest):
 
         # Now with a bogus URL
         with patch('weeutil.weeutil.input', side_effect=['y']):
-            with patch('weecfg.input', side_effect=['http://www.example.com', STATION_URL]):
+            with patch('weecfg.input', side_effect=['https://www.example.com', STATION_URL]):
                 weecfg.station_config.config_registry(self.config_dict)
         self.assertTrue(self.config_dict['StdRESTful']['StationRegistry']['register_this_station'])
         self.assertEqual(self.config_dict['Station']['station_url'], STATION_URL)
@@ -252,7 +198,8 @@ class UnitsConfigTest(CommonConfigTest):
         self.assertNotIn('unit_system', self.config_dict['StdReport']['Defaults'])
 
     def test_args_units(self):
-        weecfg.station_config.config_units(self.config_dict, unit_system='metricwx', no_prompt=True)
+        weecfg.station_config.config_units(self.config_dict, unit_system='metricwx',
+                                           no_prompt=True)
         self.assertEqual(self.config_dict['StdReport']['Defaults']['unit_system'], 'metricwx')
 
     @suppress_stdout
