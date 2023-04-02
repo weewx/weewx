31dfdc5ec419e29a53dc020017a519e8c6e6666b
Tom Keffer <tkeffer@gmail.com>
2023-04-02 10:23:52 -0700

    Fix bug in postinst
    v5.0.0a27

--- bin/weewx/__init__.py
+++ bin/weewx/__init__.py
@@ -8,7 +8,7 @@ import os.path
 import sys
 import time
 
-__version__ = "5.0.0a26"
+__version__ = "5.0.0a27"
 
 # Holds the program launch time in unix epoch seconds:
 # Useful for calculating 'uptime.'
