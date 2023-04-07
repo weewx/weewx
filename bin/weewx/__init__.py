28a18d04674789e88896a6e99d564432babf1187
Tom Keffer <tkeffer@gmail.com>
2023-04-07 05:42:56 -0700

    v5.0.0a28

--- bin/weewx/__init__.py
+++ bin/weewx/__init__.py
@@ -8,7 +8,7 @@ import os.path
 import sys
 import time
 
-__version__ = "5.0.0a27"
+__version__ = "5.0.0a28"
 
 # Holds the program launch time in unix epoch seconds:
 # Useful for calculating 'uptime.'
