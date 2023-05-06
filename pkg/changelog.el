4b9fc7d035e01942a5c5ed06988eda1e036ab23d
Matthew Wall <matthewwall@users.noreply.github.com>
2023-05-06 07:39:08 -0400

    bump version to 5.0b1

--- pkg/changelog.el
+++ pkg/changelog.el
@@ -1,3 +1,5 @@
+* Sat May 06 2023 Matthew Wall (weewx) <mwall@users.sourceforge.net> - 5.0.0b1-1
+- new upstream release
 * Sat May 06 2023 Matthew Wall (weewx) <mwall@users.sourceforge.net> - 5.0.0a30-1
 - new upstream release
 * Fri May 05 2023 Matthew Wall (weewx) <mwall@users.sourceforge.net> - 5.0.0a29-1
