b5fe70ad8ec0a12e48a5f90e0fdf47f343f9fee6
Tom Keffer <tkeffer@gmail.com>
2023-10-13 16:32:33 -0700

    Simplify implementation of get_object()

--- bin/weeutil/weeutil.py
+++ bin/weeutil/weeutil.py
@@ -14,14 +14,12 @@
 import calendar
 import cmath
 import datetime
+import importlib
 import math
 import os
 import re
 import shutil
 import time
-
-# For backwards compatibility:
-from weeutil.config import accumulateLeaves, search_up
 
 
 def convertToFloat(seq):
@@ -1345,22 +1343,10 @@ def latlon_string(ll, hemi, which, format_list=None):
 def get_object(module_class):
     """Given a string with a module class name, it imports and returns the class."""
     # Split the path into its parts
-    parts = module_class.split('.')
-    # Strip off the classname:
-    module = '.'.join(parts[:-1])
-    # Import the top level module
-    mod = __import__(module)
-    # Recursively work down from the top level module to the class name.
-    # Be prepared to catch an exception if something cannot be found.
-    try:
-        for part in parts[1:]:
-            mod = getattr(mod, part)
-    except AttributeError:
-        # Can't find something. Give a more informative error message:
-        raise AttributeError(
-            "Module '%s' has no attribute '%s' when searching for '%s'"
-            % (mod.__name__, part, module_class))
-    return mod
+    module_name, klass_name = module_class.rsplit('.', 1)
+    module = importlib.import_module(module_name)
+    klass = getattr(module, klass_name)
+    return klass
 
 
 # For backwards compatibility:
