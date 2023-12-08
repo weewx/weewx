6b89a27345c057bacb4b0971726b1bb30a04f679
matthew wall <matthewwall@users.noreply.github.com>
2023-12-07 22:13:40 -0500

    start refactoring so that tests do not leave junk behind that could influence other tests

--- src/weewx/tests/test_templates.py
+++ src/weewx/tests/test_templates.py
@@ -35,70 +35,21 @@ import gen_fake_data
 weewx.debug = 1
 
 log = logging.getLogger(__name__)
-# Set up logging using the defaults.
 weeutil.logger.setup('weetest_templates')
 
-os.environ['TZ'] = 'America/Los_Angeles'
-time.tzset()
-
-# This will use the locale specified by the environment variable 'LANG'
-# Other options are possible. See:
-# http://docs.python.org/2/library/locale.html#locale.setlocale
-locale.setlocale(locale.LC_ALL, '')
-
-# Find the configuration file. It's assumed to be in the same directory as me, so first figure
-# out where that is.
-my_dir = os.path.normpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-# The full path to the configuration file:
-config_path = os.path.join(my_dir, "testgen.conf")
-
-try:
-    config_dict = configobj.ConfigObj(config_path, file_error=True, encoding='utf-8')
-except IOError:
-    sys.stderr.write("Unable to open configuration file %s" % config_path)
-    # Reraise the exception (this will eventually cause the program to exit)
-    raise
-except configobj.ConfigObjError:
-    sys.stderr.write("Error while parsing configuration file %s" % config_path)
-    raise
-
-altitude_vt = weewx.units.ValueTuple(float(config_dict['Station']['altitude'][0]),
-                                     config_dict['Station']['altitude'][1],
-                                     'group_altitude')
-latitude = float(config_dict['Station']['latitude'])
-longitude = float(config_dict['Station']['longitude'])
-# Initialize the xtypes system for derived weather types:
-weewx.xtypes.xtypes.append(weewx.wxxtypes.WXXTypes(altitude_vt, latitude, longitude))
-
-# We test accumulator configurations as well, so initialize the Accumulator dictionary:
-weewx.accum.initialize(config_dict)
-
-# These tests also test the examples in the 'example' subdirectory.
-# Patch PYTHONPATH to find them.
-import weewx_data
-example_dir = os.path.normpath(os.path.join(os.path.dirname(weewx_data.__file__),
-                                            './examples'))
-sys.path.append(os.path.join(example_dir, './colorize'))
-sys.path.append(os.path.join(example_dir, './xstats/bin/user'))
-
-import colorize_1
-import colorize_2
-import colorize_3
-
-# Monkey patch to create SLEs with unambiguous names. We will test using these names.
-colorize_1.Colorize.colorize_1 = colorize_1.Colorize.colorize
-colorize_2.Colorize.colorize_2 = colorize_2.Colorize.colorize
-colorize_3.Colorize.colorize_3 = colorize_3.Colorize.colorize
+# global state (for now)
+settings = dict()
+config_dict = dict()
 
 
-# We will be testing the ability to extend the unit system, so set that up first:
+# We will be testing the ability to extend the unit system, so set that up
 class ExtraUnits(object):
     def __getitem__(self, obs_type):
         if obs_type.endswith('Temp'):
-            # Anything that ends with "Temp" is assumed to be in group_temperature
+            # Things that end with "Temp" is assumed to be in group_temperature
             return "group_temperature"
         elif obs_type.startswith('current'):
-            # Anything that starts with "current" is in group_amperage:
+            # Anything that starts with "current" is in group_amperage
             return "group_amperage"
         else:
             raise KeyError(obs_type)
@@ -107,48 +58,124 @@ class ExtraUnits(object):
         return obs_type.endswith('Temp') or obs_type.startswith('current')
 
 
-extra_units = ExtraUnits()
-weewx.units.obs_group_dict.extend(extra_units)
+def setup():
+    global settings
+    global config_dict
 
-# Add the new group group_amperage to the standard unit systems:
-weewx.units.USUnits["group_amperage"] = "amp"
-weewx.units.MetricUnits["group_amperage"] = "amp"
-weewx.units.MetricWXUnits["group_amperage"] = "amp"
+    # get settings that we have to reset when done
+    settings['tz'] = os.environ.get('TZ')
+    settings['locale'] = locale.getlocale()
 
-weewx.units.default_unit_format_dict["amp"] = "%.1f"
-weewx.units.default_unit_label_dict["amp"] = " A"
+    # set timezone so we have consistent information for the tests
+    os.environ['TZ'] = 'America/Los_Angeles'
+    time.tzset()
+
+    # This will use the locale specified by the environment variable 'LANG'
+    # Other options are possible. See:
+    # http://docs.python.org/2/library/locale.html#locale.setlocale
+    locale.setlocale(locale.LC_ALL, '')
+
+    # Find the configuration file. It's assumed to be in the same directory
+    # as me, so first figure out where that is.
+    my_dir = os.path.abspath(os.path.dirname(__file__))
+
+    # The full path to the configuration file:
+    config_path = os.path.join(my_dir, "testgen.conf")
+
+    config_dict = dict()
+    try:
+        config_dict = configobj.ConfigObj(config_path, file_error=True, encoding='utf-8')
+    except IOError:
+        sys.stderr.write("Unable to open configuration file %s" % config_path)
+        # Reraise the exception (this will eventually cause the program to exit)
+        raise
+    except configobj.ConfigObjError:
+        sys.stderr.write("Error while parsing configuration file %s" % config_path)
+        raise
+
+    altitude_vt = weewx.units.ValueTuple(float(config_dict['Station']['altitude'][0]),
+                                         config_dict['Station']['altitude'][1],
+                                         'group_altitude')
+    latitude = float(config_dict['Station']['latitude'])
+    longitude = float(config_dict['Station']['longitude'])
+
+    # Initialize the xtypes system for derived weather types:
+    weewx.xtypes.xtypes.append(weewx.wxxtypes.WXXTypes(altitude_vt,
+                                                       latitude, longitude))
+
+    # We test accumulator configurations as well, so initialize the Accumulator
+    # dictionary:
+    weewx.accum.initialize(config_dict)
+
+    # The tests also test the examples in the 'example' subdirectory.
+    # Patch PYTHONPATH to find them.
+    import weewx_data
+    example_dir = os.path.normpath(os.path.join(os.path.dirname(weewx_data.__file__), './examples'))
+    sys.path.append(os.path.join(example_dir, './colorize'))
+    sys.path.append(os.path.join(example_dir, './xstats/bin/user'))
+
+    import colorize_1
+    import colorize_2
+    import colorize_3
+
+    # Monkey patch to create SLEs with unambiguous names. We will test using
+    # these names.
+    colorize_1.Colorize.colorize_1 = colorize_1.Colorize.colorize
+    colorize_2.Colorize.colorize_2 = colorize_2.Colorize.colorize
+    colorize_3.Colorize.colorize_3 = colorize_3.Colorize.colorize
+
+    # add the units we will use in the tests
+    extra_units = ExtraUnits()
+    weewx.units.obs_group_dict.extend(extra_units)
+
+    # Add the new group group_amperage to the standard unit systems:
+    weewx.units.USUnits["group_amperage"] = "amp"
+    weewx.units.MetricUnits["group_amperage"] = "amp"
+    weewx.units.MetricWXUnits["group_amperage"] = "amp"
+    weewx.units.default_unit_format_dict["amp"] = "%.1f"
+    weewx.units.default_unit_label_dict["amp"] = " A"
+
+
+def teardown():
+    global settings
+    print("settings: %s" % settings)
+    # put things back to the way they were before we started
+    locale.setlocale(locale.LC_ALL, settings['locale'])
+    os.environ['TZ'] = settings['tz']
+    time.tzset()
+
+    # reset xtypes
+#    weewx.xtypes.xtypes.append(weewx.wxxtypes.WXXTypes(altitude_vt,
+#                                                       latitude, longitude))
+
+    # reset accum
+#    weewx.accum.initialize(config_dict)
+
+    # reset units
+#    weewx.units.obs_group_dict.extend(extra_units)
 
 
 class Common(object):
 
     def setUp(self):
-        global config_dict
-
         self.config_dict = weeutil.config.deep_copy(config_dict)
-
-        # Remove the old directory:
-        try:
-            test_html_dir = os.path.join(self.config_dict['WEEWX_ROOT'],
-                                         self.config_dict['StdReport']['HTML_ROOT'])
-            shutil.rmtree(test_html_dir)
-        except OSError as e:
-            if os.path.exists(test_html_dir):
-                print("\nUnable to remove old test directory %s", test_html_dir, file=sys.stderr)
-                print("Reason:", e, file=sys.stderr)
-                print("Aborting", file=sys.stderr)
-                exit(1)
-
-        # This will generate the test databases if necessary:
+        # be sure no one else left things behind that will conflict
+        root_dir = self.config_dict['WEEWX_ROOT']
+        shutil.rmtree(root_dir, ignore_errors=True)
+        # generate the test databases
         gen_fake_data.configDatabases(self.config_dict, database_type=self.database_type)
 
     def tearDown(self):
-        pass
+        # clean up after ourself
+        root_dir = self.config_dict['WEEWX_ROOT']
+        shutil.rmtree(root_dir, ignore_errors=True)
 
     def test_report_engine(self):
 
-        # The generation time should be the same as the last record in the test database:
+        # The generation time should be the same as the last record in the test
+        # database:
         testtime_ts = gen_fake_data.stop_ts
-        print("\ntest time is %s" % weeutil.weeutil.timestamp_to_string(testtime_ts))
+        print("test time is %s" % weeutil.weeutil.timestamp_to_string(testtime_ts))
 
         stn_info = weewx.station.StationInfo(**self.config_dict['Station'])
 
@@ -162,25 +189,25 @@ class Common(object):
     def run_engine(self, stn_info, record, testtime_ts):
         t = weewx.reportengine.StdReportEngine(self.config_dict, stn_info, record, testtime_ts)
 
-        # Find the test skins and then have SKIN_ROOT point to it:
-        test_dir = sys.path[0]
-        t.config_dict['StdReport']['SKIN_ROOT'] = os.path.join(test_dir, 'test_skins')
+        # Make SKIN_ROOT point to the correct location
+        t.config_dict['StdReport']['SKIN_ROOT'] = os.path.join(DIR, 'test_skins')
 
-        # Although the report engine inherits from Thread, we can just run it in the main thread:
+        # Although the report engine inherits from Thread, we can just run it
+        # in the main thread:
         print("Starting report engine test")
         t.run()
         print("Done.")
 
         test_html_dir = os.path.join(t.config_dict['WEEWX_ROOT'],
                                      t.config_dict['StdReport']['HTML_ROOT'])
-        expected_dir = os.path.join(test_dir, 'expected')
+        expected_dir = os.path.join(DIR, 'expected')
 
-        # Walk the directory of expected results to discover all the generated files we should
-        # be checking
+        # Walk the directory of expected results to discover all the generated
+        # files we should be checking
         for dirpath, _, dirfilenames in os.walk(expected_dir):
             for dirfilename in dirfilenames:
                 expected_filename_abs = os.path.join(dirpath, dirfilename)
-                # Get the file path relative to the directory of expected results
+                # Get the path relative to the directory of expected results
                 filename_rel = os.path.relpath(expected_filename_abs, expected_dir)
                 # Use that to figure out where the actual results ended up
                 actual_filename_abs = os.path.join(test_html_dir, filename_rel)
@@ -194,11 +221,8 @@ class Common(object):
                             if actual_line == '' and expected_line == '':
                                 break
                             n += 1
-                            self.assertEqual(actual_line,
-                                             expected_line,
-                                             msg="%s[%d]:\n%r vs\n%r"
-                                                 % (actual_filename_abs, n, actual_line,
-                                                    expected_line))
+                            msg = "%s[%d]" % (actual_filename_abs, n)
+                            self.assertEqual(actual_line, expected_line, msg=msg)
 
                         print(f"Checked {n:d} lines in {filename_rel}")
 
