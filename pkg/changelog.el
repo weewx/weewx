e546d93f2fff980166fe58217223d52375fcbe72
Matthew Wall <matthewwall@users.noreply.github.com>
2023-12-14 08:17:51 -0500

    update package changelogs

--- pkg/changelog.el
+++ pkg/changelog.el
@@ -1,3 +1,6 @@
+* Tue Dec 13 2023 Matthew Wall (weewx) <mwall@users.sourceforge.net> - 5.0.0b17-4
+- remove bytecompiled code before removing package
+- do not enable template unit
 * Tue Dec 13 2023 Matthew Wall (weewx) <mwall@users.sourceforge.net> - 5.0.0b17-3
 - fix typo in config file macro name
 * Tue Dec 13 2023 Matthew Wall (weewx) <mwall@users.sourceforge.net> - 5.0.0b17-2
