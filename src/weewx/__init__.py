5244ff22ad8642b415bd2de438b06b5db0a71e7e
matthew wall <matthewwall@users.noreply.github.com>
2023-12-29 08:40:02 -0500

    bump to 5.0.0rc2

--- src/weewx/__init__.py
+++ src/weewx/__init__.py
@@ -6,7 +6,7 @@
 """Package weewx, containing modules specific to the weewx runtime engine."""
 import time
 
-__version__ = "5.0.0rc1"
+__version__ = "5.0.0rc2"
 
 # Holds the program launch time in unix epoch seconds:
 # Useful for calculating 'uptime.'
