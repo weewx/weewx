3625c02752c3f78c75893d557b7d6c0a646aa093
Tom Keffer <tkeffer@gmail.com>
2024-02-24 15:20:49 -0800

    Use the first xtype that returns a result for not_null

--- src/weewx/xtypes.py
+++ src/weewx/xtypes.py
@@ -155,8 +155,7 @@ def has_data(obs_type, timespan, db_manager):
             # aggregation.
             vt = xtype.get_aggregate(obs_type, timespan, 'not_null', db_manager)
             # Check to see if we found a non-null value. Otherwise, keep going.
-            if vt[0]:
-                return True
+            return bool(vt[0])
         except (weewx.UnknownType, weewx.UnknownAggregation):
             pass
         except weewx.CannotCalculate:
@@ -166,12 +165,6 @@ def has_data(obs_type, timespan, db_manager):
     # Tried all the  get_aggregates() and didn't find a non-null value. Either it doesn't exist,
     # or doesn't have any data
     return False
-
-    # try:
-    #     vt = get_aggregate(obs_type, timespan, 'not_null', db_manager)
-    #     return bool(vt[0])
-    # except (weewx.UnknownAggregation, weewx.UnknownType):
-    #     return False
 
 
 #
