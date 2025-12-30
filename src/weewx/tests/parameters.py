1847cb0563e5a954a3e5e80fa18ce858cee87410
Tom Keffer <tkeffer@gmail.com>
2025-12-30 12:08:07 -0800

    Start using pytest.

---
+++ src/weewx/tests/parameters.py
@@ -0,0 +1,32 @@
+#
+#    Copyright (c) 2025 Tom Keffer <tkeffer@gmail.com>
+#
+#    See the file LICENSE.txt for your full rights.
+#
+"""Parameters used for database-related tests"""
+import os
+import time
+
+os.environ['TZ'] = 'America/Los_Angeles'
+time.tzset()
+
+# The start of the 'solar year' for 2009-2010
+year_start_tt = (2009, 12, 21, 9, 47, 0, 0, 0, 0)
+year_start = int(time.mktime(year_start_tt))
+
+# Roughly nine months of data:
+start_tt = (2010, 1, 1, 0, 0, 0, 0, 0, -1)  # 2010-01-01 00:00
+stop_tt = (2010, 9, 3, 11, 0, 0, 0, 0, -1)  # 2010-09-03 11:00
+alt_start_tt = (2010, 8, 30, 0, 0, 0, 0, 0, -1)
+start_ts = int(time.mktime(start_tt))
+stop_ts = int(time.mktime(stop_tt))
+alt_start = int(time.mktime(alt_start_tt))
+interval = 1800
+
+synthetic_dict = {
+    'start_ts': start_ts,
+    'stop_ts': stop_ts,
+    'interval': interval,
+}
+alt_dict = dict(synthetic_dict)
+alt_dict['amplitude'] = 0.5
