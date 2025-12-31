516073dae216941bd452b006169155db1cf596f0
Tom Keffer <tkeffer@gmail.com>
2025-12-31 13:34:38 -0800

    Convert weeutil test modules

--- src/weeutil/tests/test_config.py
+++ src/weeutil/tests/test_config.py
@@ -1,15 +1,14 @@
-# coding: utf-8
 #
-#    Copyright (c) 2020-2024 Tom Keffer <tkeffer@gmail.com>
+#    Copyright (c) 2020-2026 Tom Keffer <tkeffer@gmail.com>
 #
 #    See the file LICENSE.txt for your full rights.
 #
 """Test module weeutil.config"""
 import logging
-import unittest
 from io import BytesIO, StringIO
 
 import configobj
+import pytest
 
 import weeutil.config
 import weeutil.logger
@@ -23,16 +22,14 @@ log = logging.getLogger(__name__)
 weeutil.logger.setup('weetest_config')
 
 
-class TestConfigString(unittest.TestCase):
-
-    def test_config_from_str(self):
-        test_str = """degree_C = °C"""
-        c = weeutil.config.config_from_str(test_str)
-        # Make sure the values are Unicode
-        self.assertEqual(type(c['degree_C']), str)
+def test_config_from_str():
+    test_str = """degree_C = °C"""
+    c = weeutil.config.config_from_str(test_str)
+    # Make sure the values are Unicode
+    assert type(c['degree_C']) is str
 
 
-class TestConfig(unittest.TestCase):
+class TestConfig:
     test_dict_str = u"""[Logging]
     [[formatters]]
         [[[simple]]]
@@ -45,53 +42,49 @@ class TestConfig(unittest.TestCase):
             datefmt = %Y-%m-%d %H:%M:%S
 """
 
-    def setUp(self):
+    @pytest.fixture
+    def c_in(self):
         test_dict = StringIO(TestConfig.test_dict_str)
-        self.c_in = configobj.ConfigObj(test_dict, encoding='utf-8', default_encoding='utf-8')
+        return configobj.ConfigObj(test_dict, encoding='utf-8', default_encoding='utf-8')
 
-    def test_deep_copy_ConfigObj(self):
+    def test_deep_copy_ConfigObj(self, c_in):
         """Test copying a full ConfigObj"""
 
-        c_out = weeutil.config.deep_copy(self.c_in)
-        self.assertIsInstance(c_out, configobj.ConfigObj)
-        self.assertEqual(c_out, self.c_in)
+        c_out = weeutil.config.deep_copy(c_in)
+        assert isinstance(c_out, configobj.ConfigObj)
+        assert c_out == c_in
 
         # Make sure the parentage is correct
-        self.assertIs(c_out['Logging']['formatters'].parent, c_out['Logging'])
-        self.assertIs(c_out['Logging']['formatters'].parent.parent, c_out)
-        self.assertIs(c_out['Logging']['formatters'].main, c_out)
-        self.assertIsNot(c_out['Logging']['formatters'].main, self.c_in)
-        self.assertIs(c_out.main, c_out)
-        self.assertIsNot(c_out.main, self.c_in)
+        assert c_out['Logging']['formatters'].parent is c_out['Logging']
+        assert c_out['Logging']['formatters'].parent.parent is c_out
+        assert c_out['Logging']['formatters'].main is c_out
+        assert c_out['Logging']['formatters'].main is not c_in
+        assert c_out.main is c_out
+        assert c_out.main is not c_in
 
         # Try changing something and see if it's still equal:
         c_out['Logging']['formatters']['verbose']['datefmt'] = 'foo'
-        self.assertNotEqual(c_out, self.c_in)
+        assert c_out != c_in
         # The original ConfigObj entry should still be the same
-        self.assertEqual(self.c_in['Logging']['formatters']['verbose']['datefmt'],
-                         '%Y-%m-%d %H:%M:%S')
+        assert c_in['Logging']['formatters']['verbose']['datefmt'] == '%Y-%m-%d %H:%M:%S'
 
-    def test_deep_copy_Section(self):
+    def test_deep_copy_Section(self, c_in):
         """Test copying just a section"""
-        c_out = weeutil.config.deep_copy(self.c_in['Logging']['formatters'])
-        self.assertNotIsInstance(c_out, configobj.ConfigObj)
-        self.assertIsInstance(c_out, configobj.Section)
-        self.assertEqual(c_out, self.c_in['Logging']['formatters'])
+        c_out = weeutil.config.deep_copy(c_in['Logging']['formatters'])
+        assert not isinstance(c_out, configobj.ConfigObj)
+        assert isinstance(c_out, configobj.Section)
+        assert c_out == c_in['Logging']['formatters']
 
         # Check parentage
-        self.assertIs(c_out.main, self.c_in)
-        self.assertIs(c_out.parent, self.c_in['Logging'])
-        self.assertIs(c_out['verbose'].parent, c_out)
-        self.assertIs(c_out['verbose'].parent.parent, self.c_in['Logging'])
+        assert c_out.main is c_in
+        assert c_out.parent is c_in['Logging']
+        assert c_out['verbose'].parent is c_out
+        assert c_out['verbose'].parent.parent is c_in['Logging']
 
-    def test_deep_copy_write(self):
-        c_out = weeutil.config.deep_copy(self.c_in)
+    def test_deep_copy_write(self, c_in):
+        c_out = weeutil.config.deep_copy(c_in)
         bio = BytesIO()
         c_out.write(bio)
         bio.seek(0)
         out_str = bio.read().decode('utf-8')
-        self.assertEqual(out_str, TestConfig.test_dict_str)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert out_str == TestConfig.test_dict_str
--- src/weeutil/tests/test_sun.py
+++ src/weeutil/tests/test_sun.py
@@ -1,32 +1,26 @@
 #
-#    Copyright (c) 2018 Tom Keffer <tkeffer@gmail.com>
+#    Copyright (c) 2018-2026 Tom Keffer <tkeffer@gmail.com>
 #
 #    See the file LICENSE.txt for your full rights.
 #
 import os
 import time
-import unittest
+import pytest
 
 from weeutil import Sun
 
 
-class SunTest(unittest.TestCase):
+def test_sun_rise_set():
+    os.environ['TZ'] = 'Australia/Sydney'
+    time.tzset()
+    # Sydney, Australia
+    result = Sun.sunRiseSet(2012, 1, 1, 151.21, -33.86)
+    assert result[0] == pytest.approx(-5.223949864965772, abs=1e-6)
+    assert result[1] == pytest.approx(9.152208948206106, abs=1e-6)
 
-    def test_sunRiseSet(self):
-        os.environ['TZ'] = 'Australia/Sydney'
-        time.tzset()
-        # Sydney, Australia
-        result = Sun.sunRiseSet(2012, 1, 1, 151.21, -33.86)
-        self.assertAlmostEqual(result[0], -5.223949864965772, 6)
-        self.assertAlmostEqual(result[1], 9.152208948206106, 6)
-
-        os.environ['TZ'] = 'America/Los_Angeles'
-        time.tzset()
-        # Hood River, USA
-        result = Sun.sunRiseSet(2012, 1, 1, -121.566, 45.686)
-        self.assertAlmostEqual(result[0], 15.781521580780003, 6)
-        self.assertAlmostEqual(result[1], 24.528947667456983, 6)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    os.environ['TZ'] = 'America/Los_Angeles'
+    time.tzset()
+    # Hood River, USA
+    result = Sun.sunRiseSet(2012, 1, 1, -121.566, 45.686)
+    assert result[0] == pytest.approx(15.781521580780003, abs=1e-6)
+    assert result[1] == pytest.approx(24.528947667456983, abs=1e-6)
