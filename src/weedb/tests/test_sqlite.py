f986c46bf63717d199b747920a5dba3669828505
Tom Keffer <tkeffer@gmail.com>
2025-12-31 12:17:55 -0800

    Rename check_mysql and check_sqlite. Convert to pytest

--- src/weedb/tests/test_sqlite.py
+++ src/weedb/tests/test_sqlite.py
@@ -1,5 +1,5 @@
 #
-#    Copyright (c) 2009-2024 Tom Keffer <tkeffer@gmail.com>
+#    Copyright (c) 2009-2026 Tom Keffer <tkeffer@gmail.com>
 #
 #    See the file LICENSE.txt for your full rights.
 #
@@ -12,32 +12,13 @@
 
 import os
 import sqlite3
-import sys
-import unittest
+import pytest
 from sqlite3 import IntegrityError, OperationalError
 
 # This database should be somewhere where you have write permissions:
 sqdb1 = '/var/tmp/sqdb1.sdb'
 # This database should be somewhere where you do NOT have write permissions:
 sqdb2 = '/usr/local/sqdb2.sdb'
-
-# Double check that we have the necessary permissions (or lack thereof):
-try:
-    fd = open(sqdb1, 'w')
-    fd.close()
-except:
-    print("For tests to work properly, you must have write permission to '%s'." % sqdb1, file=sys.stderr)
-    print("Change the permissions and try again.", file=sys.stderr)
-    sys.exit("Must have write permissions to '%s'" % sqdb1)
-try:
-    fd = open(sqdb2, 'w')
-    fd.close()
-except IOError:
-    pass
-else:
-    print("For tests to work properly, you must NOT have write permission to '%s'." % sqdb2, file=sys.stderr)
-    print("Change the permissions and try again.", file=sys.stderr)
-    sys.exit("Must not have write permissions to '%s'" % sqdb2)
 
 
 class Cursor:
@@ -55,14 +36,16 @@ class Cursor:
         self.connection.close()
 
 
-class TestSqlite3(unittest.TestCase):
+class TestSqlite3:
 
-    def setUp(self):
-        """Make sure sqdb1 and sqdb2 are gone."""
-        self.tearDown()
-
-    def tearDown(self):
+    @pytest.fixture(autouse=True)
+    def setup_teardown(self):
         """Remove any databases we created."""
+        self.tear_down()
+        yield
+        self.tear_down()
+
+    def tear_down(self):
         try:
             os.remove(sqdb1)
         except OSError:
@@ -73,39 +56,35 @@ class TestSqlite3(unittest.TestCase):
             pass
 
     def test_create_nopermission(self):
-        with self.assertRaises(OperationalError) as e:
-            with Cursor(sqdb2) as cursor:
+        with pytest.raises(OperationalError) as e:
+            with Cursor(sqdb2):
                 pass
-        self.assertEqual(str(e.exception), 'unable to open database file')
+        assert str(e.value) == 'unable to open database file'
 
     def test_select_nonexistent_table(self):
         with Cursor(sqdb1) as cursor:
-            with self.assertRaises(OperationalError) as e:
+            with pytest.raises(OperationalError) as e:
                 cursor.execute("SELECT foo from bar")
-            self.assertEqual(str(e.exception), "no such table: bar")
+            assert str(e.value) == "no such table: bar"
 
     def test_double_table_create(self):
         with Cursor(sqdb1) as cursor:
             cursor.execute("CREATE TABLE bar (col1 int, col2 int)")
-            with self.assertRaises(OperationalError) as e:
+            with pytest.raises(OperationalError) as e:
                 cursor.execute("CREATE TABLE bar (col1 int, col2 int)")
-            self.assertEqual(str(e.exception), 'table bar already exists')
+            assert str(e.value) == 'table bar already exists'
 
     def test_select_nonexistent_column(self):
         with Cursor(sqdb1) as cursor:
             cursor.execute("CREATE TABLE bar (col1 int, col2 int)")
-            with self.assertRaises(OperationalError) as e:
+            with pytest.raises(OperationalError) as e:
                 cursor.execute("SELECT foo from bar")
-            self.assertEqual(str(e.exception), 'no such column: foo')
+            assert str(e.value) == 'no such column: foo'
 
     def test_duplicate_key(self):
         with Cursor(sqdb1) as cursor:
             cursor.execute("CREATE TABLE test1 ( dateTime INTEGER NOT NULL PRIMARY KEY, col1 int, col2 int)")
             cursor.execute("INSERT INTO test1 (dateTime, col1, col2) VALUES (1, 10, 20)")
-            with self.assertRaises(IntegrityError) as e:
+            with pytest.raises(IntegrityError) as e:
                 cursor.execute("INSERT INTO test1 (dateTime, col1, col2) VALUES (1, 30, 40)")
-            self.assertEqual(str(e.exception), "UNIQUE constraint failed: test1.dateTime")
-
-
-if __name__ == '__main__':
-    unittest.main()
+            assert str(e.value) == "UNIQUE constraint failed: test1.dateTime"
