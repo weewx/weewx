6e6d69f0d9f9f86f898c64debad9b2642804ec0b
Tom Keffer <tkeffer@gmail.com>
2025-12-31 05:00:39 -0800

    Convert test_upgrade_config.py to pytest

--- src/weecfg/tests/test_upgrade_config.py
+++ src/weecfg/tests/test_upgrade_config.py
@@ -1,12 +1,12 @@
 #
-#    Copyright (c) 2009-2024 Tom Keffer <tkeffer@gmail.com>
+#    Copyright (c) 2009-2026 Tom Keffer <tkeffer@gmail.com>
 #
 #    See the file LICENSE.txt for your full rights.
 #
 """Test the utilities that upgrade the configuration file."""
 import io
 import os.path
-import unittest
+import pytest
 import tempfile
 
 import configobj
@@ -24,17 +24,53 @@ def check_fileend(out_str):
             out_str.write('\n')
 
 
-# Change directory so we can find things dependent on the location of
-# this file, such as config files and expected values:
-this_file = os.path.join(os.getcwd(), __file__)
-this_dir = os.path.abspath(os.path.dirname(this_file))
-os.chdir(this_dir)
+@pytest.fixture(autouse=True)
+def setup_dir():
+    """Change directory so we can find things dependent on the location of
+    this file, such as config files and expected values:"""
+    this_file = os.path.join(os.getcwd(), __file__)
+    this_dir = os.path.abspath(os.path.dirname(this_file))
+    old_dir = os.getcwd()
+    os.chdir(this_dir)
+    yield
+    os.chdir(old_dir)
 
 
 current_config_dict_path = "../../weewx_data/weewx.conf"
 
 
-class ConfigTest(unittest.TestCase):
+def _check_against_expected(config_dict, expected):
+    """Check a ConfigObj against an expected version
+
+    config_dict: The ConfigObj that is to be checked
+
+    expected: The name of a file holding the expected version
+    """
+    # Writing a ConfigObj to a file-like object always writes in bytes,
+    # so we cannot write to a StringIO (which accepts only Unicode under Python 3).
+    # Use a BytesIO object instead, which accepts byte strings.
+    with io.BytesIO() as fd_actual:
+        config_dict.write(fd_actual)
+        check_fileend(fd_actual)
+        fd_actual.seek(0)
+
+        # When we read the BytesIO object back in, the results will be in byte strings.
+        # To compare apples-to-apples, we need to open the file with expected
+        # strings in binary, so when we read it, we get byte-strings:
+        with open(expected, 'rb') as fd_expected:
+            n = 0
+            for expected_line in fd_expected:
+                actual_line = fd_actual.readline()
+                n += 1
+                assert actual_line.strip() == expected_line.strip(), \
+                    "[%d] '%s' vs '%s'" % (n, actual_line, expected_line)
+
+            # Make sure there are no extra lines in the updated config:
+            more = fd_actual.readline()
+            assert more == b''
+
+
+class TestUpgrade:
 
     def test_upgrade_v25(self):
         # Start with the Version 2.0 weewx.conf file:
@@ -43,7 +79,7 @@ class ConfigTest(unittest.TestCase):
         # Upgrade the V2.0 configuration dictionary to V2.5:
         weecfg.update_config.update_to_v25(config_dict)
 
-        self._check_against_expected(config_dict, 'expected/weewx25_expected.conf')
+        _check_against_expected(config_dict, 'expected/weewx25_expected.conf')
 
     def test_upgrade_v26(self):
         # Start with the Version 2.5 weewx.conf file:
@@ -52,7 +88,7 @@ class ConfigTest(unittest.TestCase):
         # Upgrade the V2.5 configuration dictionary to V2.6:
         weecfg.update_config.update_to_v26(config_dict)
 
-        self._check_against_expected(config_dict, 'expected/weewx26_expected.conf')
+        _check_against_expected(config_dict, 'expected/weewx26_expected.conf')
 
     def test_upgrade_v30(self):
         # Start with the Version 2.7 weewx.conf file:
@@ -64,7 +100,7 @@ class ConfigTest(unittest.TestCase):
         # with open('expected/weewx30_expected.conf', 'wb') as fd:
         #     config_dict.write(fd)
 
-        self._check_against_expected(config_dict, 'expected/weewx30_expected.conf')
+        _check_against_expected(config_dict, 'expected/weewx30_expected.conf')
 
     def test_upgrade_v32(self):
         # Start with the Version 3.0 weewx.conf file:
@@ -76,7 +112,7 @@ class ConfigTest(unittest.TestCase):
         # with open('expected/weewx32_expected.conf', 'wb') as fd:
         #     config_dict.write(fd)
 
-        self._check_against_expected(config_dict, 'expected/weewx32_expected.conf')
+        _check_against_expected(config_dict, 'expected/weewx32_expected.conf')
 
     def test_upgrade_v36(self):
         # Start with the Version 3.2 weewx.conf file:
@@ -85,7 +121,7 @@ class ConfigTest(unittest.TestCase):
         # Upgrade the V3.2 configuration dictionary to V3.6:
         weecfg.update_config.update_to_v36(config_dict)
 
-        self._check_against_expected(config_dict, 'expected/weewx36_expected.conf')
+        _check_against_expected(config_dict, 'expected/weewx36_expected.conf')
 
     def test_upgrade_v39(self):
         # Start with the Version 3.8 weewx.conf file:
@@ -97,7 +133,7 @@ class ConfigTest(unittest.TestCase):
         # with open('expected/weewx39_expected.conf', 'wb') as fd:
         #     config_dict.write(fd)
 
-        self._check_against_expected(config_dict, 'expected/weewx39_expected.conf')
+        _check_against_expected(config_dict, 'expected/weewx39_expected.conf')
 
     def test_upgrade_v40(self):
         """Test an upgrade of the stock v3.9 weewx.conf to V4.0"""
@@ -111,7 +147,7 @@ class ConfigTest(unittest.TestCase):
         # with open('expected/weewx40_expected.conf', 'wb') as fd:
         #     config_dict.write(fd)
 
-        self._check_against_expected(config_dict, 'expected/weewx40_expected.conf')
+        _check_against_expected(config_dict, 'expected/weewx40_expected.conf')
 
     def test_upgrade_v42(self):
         """Test an upgrade of the stock v4.1 weewx.conf to V4.2"""
@@ -125,10 +161,10 @@ class ConfigTest(unittest.TestCase):
         # with open('expected/weewx42_expected.conf', 'wb') as fd:
         #     config_dict.write(fd)
 
-        self._check_against_expected(config_dict, 'expected/weewx42_expected.conf')
+        _check_against_expected(config_dict, 'expected/weewx42_expected.conf')
 
     def test_upgrade_v43(self):
-        """Test an upgrade of the stock v4.1 weewx.conf to V4.2"""
+        """Test an upgrade of the stock v4.2 weewx.conf to V4.3"""
 
         # Start with the Version 4.2 weewx.conf file:
         config_dict = configobj.ConfigObj('weewx42.conf', encoding='utf-8')
@@ -139,7 +175,7 @@ class ConfigTest(unittest.TestCase):
         # with open('expected/weewx43_expected.conf', 'wb') as fd:
         #     config_dict.write(fd)
 
-        self._check_against_expected(config_dict, 'expected/weewx43_expected.conf')
+        _check_against_expected(config_dict, 'expected/weewx43_expected.conf')
 
     def test_merge(self):
         """Test an upgrade against a typical user's configuration file"""
@@ -162,38 +198,4 @@ class ConfigTest(unittest.TestCase):
         #     check_dict.write(fd)
 
         # Check the results.
-        self._check_against_expected(check_dict, 'expected/weewx43_user_expected.conf')
-
-    def _check_against_expected(self, config_dict, expected):
-        """Check a ConfigObj against an expected version
-
-        config_dict: The ConfigObj that is to be checked
-
-        expected: The name of a file holding the expected version
-        """
-        # Writing a ConfigObj to a file-like object always writes in bytes,
-        # so we cannot write to a StringIO (which accepts only Unicode under Python 3).
-        # Use a BytesIO object instead, which accepts byte strings.
-        with io.BytesIO() as fd_actual:
-            config_dict.write(fd_actual)
-            check_fileend(fd_actual)
-            fd_actual.seek(0)
-
-            # When we read the BytesIO object back in, the results will be in byte strings.
-            # To compare apples-to-apples, we need to open the file with expected
-            # strings in binary, so when we read it, we get byte-strings:
-            with open(expected, 'rb') as fd_expected:
-                N = 0
-                for expected in fd_expected:
-                    actual = fd_actual.readline()
-                    N += 1
-                    self.assertEqual(actual.strip(), expected.strip(),
-                                     "[%d] '%s' vs '%s'" % (N, actual, expected))
-
-                # Make sure there are no extra lines in the updated config:
-                more = fd_actual.readline()
-                self.assertEqual(more, b'')
-
-
-if __name__ == "__main__":
-    unittest.main()
+        _check_against_expected(check_dict, 'expected/weewx43_user_expected.conf')
