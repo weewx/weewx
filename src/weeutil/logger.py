26fd458a3d85fd29724fa26c14b47008238fc82f
Tom Keffer <tkeffer@gmail.com>
2026-01-04 06:58:48 -0800

    Revert "Introduce option `real_as_double`"
    This reverts commit b59b7e36ddbfa869e8f254ecb6660972899012bc.

--- src/weeutil/logger.py
+++ src/weeutil/logger.py
@@ -26,7 +26,7 @@ LOGGING_STR = """[Logging]
     # Root logger
     [[root]]
       level = {log_level}
-      handlers = syslog, console
+      handlers = syslog,
 
     # Additional loggers would go in the following section. This is useful for 
     # tailoring logging for individual modules.
